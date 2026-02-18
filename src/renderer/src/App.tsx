d7200c5bbdae2210de4ef618915806cfb347f36d
harald bregu <harald.bregu@gmail.com>
2026-02-18 20:12:33 +0100

    update

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -21,33 +21,35 @@ import UpdateSimulatorPage from './pages/UpdateSimulatorPage'
 import SettingsPage from './pages/SettingsPage'
 import RagPage from './pages/RagPage'
 import './index.css'
+import HomePage from './pages/HomePage'
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-    <Router>
-      <AppLayout>
-        <Routes>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/welcome" element={<WelcomePage />} />
-          <Route path="/microphone" element={<MicrophonePage />} />
-          <Route path="/camera" element={<CameraPage />} />
-          <Route path="/screen" element={<ScreenPage />} />
-          <Route path="/bluetooth" element={<BluetoothPage />} />
-          <Route path="/network" element={<NetworkPage />} />
-          <Route path="/cron" element={<CronPage />} />
-          <Route path="/lifecycle" element={<LifecyclePage />} />
-          <Route path="/windows" element={<WindowManagerPage />} />
-          <Route path="/filesystem" element={<FilesystemPage />} />
-          <Route path="/dialogs" element={<DialogsPage />} />
-          <Route path="/notifications" element={<NotificationsPage />} />
-          <Route path="/clipboard" element={<ClipboardPage />} />
-          <Route path="/update-simulator" element={<UpdateSimulatorPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-          <Route path="/rag" element={<RagPage />} />
-        </Routes>
-      </AppLayout>
-    </Router>
+      <Router>
+        <AppLayout>
+          <Routes>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/welcome" element={<WelcomePage />} />
+            <Route path="/microphone" element={<MicrophonePage />} />
+            <Route path="/camera" element={<CameraPage />} />
+            <Route path="/screen" element={<ScreenPage />} />
+            <Route path="/bluetooth" element={<BluetoothPage />} />
+            <Route path="/network" element={<NetworkPage />} />
+            <Route path="/cron" element={<CronPage />} />
+            <Route path="/lifecycle" element={<LifecyclePage />} />
+            <Route path="/windows" element={<WindowManagerPage />} />
+            <Route path="/filesystem" element={<FilesystemPage />} />
+            <Route path="/dialogs" element={<DialogsPage />} />
+            <Route path="/notifications" element={<NotificationsPage />} />
+            <Route path="/clipboard" element={<ClipboardPage />} />
+            <Route path="/update-simulator" element={<UpdateSimulatorPage />} />
+            <Route path="/settings" element={<SettingsPage />} />
+            <Route path="/rag" element={<RagPage />} />
+          </Routes>
+        </AppLayout>
+      </Router>
     </Provider>
   )
 }
