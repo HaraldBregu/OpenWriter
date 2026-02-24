ddd0522e97d25cb05e4428e80b26fdc9b13bee32
HaraldBregu <harald.bregu@gmail.com>
2026-02-24 12:45:14 +0100

    Auto-commit from Claude Code [2026-02-24 12:45:14]

--- src/renderer/src/pages/FilesystemPage.tsx
+++ src/renderer/src/pages/FilesystemPage.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useFilesystem } from '../hooks/useFilesystem'
-import { useTheme } from '../hooks/useTheme'
 import { useLanguage } from '../hooks/useLanguage'
 import {
   FolderOpen,
