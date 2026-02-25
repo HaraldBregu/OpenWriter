a7332f3c285b46a630c82469f8a6ce73d66a2959
harald bregu <harald.bregu@gmail.com>
2026-02-25 22:04:47 +0100

    Pass eventBus to ExecutorBase parent constructor

--- src/main/pipeline/PipelineService.ts
+++ src/main/pipeline/PipelineService.ts
@@ -30,8 +30,10 @@ export class PipelineService extends ExecutorBase<PipelineRun> {
 
   constructor(
     private readonly registry: AgentRegistry,
-    private readonly eventBus: EventBus
-  ) {}
+    eventBus: EventBus
+  ) {
+    super(eventBus)
+  }
 
   // -------------------------------------------------------------------------
   // Public API
