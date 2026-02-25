6b2f139161090e019a9ffca6c06fee0322b46f6c
HaraldBregu <harald.bregu@gmail.com>
2026-02-25 09:22:06 +0100

    Add useTranslation import to ModelsSettings

--- src/renderer/src/pages/settings/ModelsSettings.tsx
+++ src/renderer/src/pages/settings/ModelsSettings.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback, useRef } from 'react'
+import { useTranslation } from 'react-i18next'
 import { aiProviders } from '@/config/aiProviders'
 
 const ModelsSettings: React.FC = () => {
