b3165d2d4fce2952744deebdf9f2c469fa1926f6
HaraldBregu <harald.bregu@gmail.com>
2026-02-25 09:24:02 +0100

    Add useInferenceSettings hook for managing settings

--- src/renderer/src/pages/settings/DevicesSettings.tsx
+++ src/renderer/src/pages/settings/DevicesSettings.tsx
@@ -1,4 +1,5 @@
 import React, { lazy, Suspense } from 'react'
+import { useTranslation } from 'react-i18next'
 import { CollapsibleSection } from './CollapsibleSection'
 import { LoadingSkeleton } from '../../components/LoadingSkeleton'
 
@@ -6,14 +7,16 @@ const BluetoothPage = lazy(() => import('../BluetoothPage'))
 const NetworkPage = lazy(() => import('../NetworkPage'))
 
 const DevicesSettings: React.FC = () => {
+  const { t } = useTranslation()
+
   return (
     <div className="divide-y">
-      <CollapsibleSection title="Bluetooth">
+      <CollapsibleSection title={t('settings.sections.bluetooth')}>
         <Suspense fallback={<LoadingSkeleton />}>
           <BluetoothPage />
         </Suspense>
       </CollapsibleSection>
-      <CollapsibleSection title="Network">
+      <CollapsibleSection title={t('settings.sections.network')}>
         <Suspense fallback={<LoadingSkeleton />}>
           <NetworkPage />
         </Suspense>
