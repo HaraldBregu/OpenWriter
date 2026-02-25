6093561c7c4168a12d2f17cc65a506d4026871e2
harald bregu <harald.bregu@gmail.com>
2026-02-25 22:15:53 +0100

    Add ProviderResolver import to AIEnhanceHandler

--- src/main/tasks/handlers/AIEnhanceHandler.ts
+++ src/main/tasks/handlers/AIEnhanceHandler.ts
@@ -10,6 +10,7 @@ import { HumanMessage, SystemMessage } from '@langchain/core/messages'
 import type { TaskHandler, ProgressReporter, StreamReporter } from '../TaskHandler'
 import type { StoreService } from '../../services/store'
 import { isReasoningModel, extractTokenFromChunk, classifyError, toUserMessage } from '../../shared/aiUtils'
+import { ProviderResolver } from '../../shared/ProviderResolver'
 
 // ---------------------------------------------------------------------------
 // Types
