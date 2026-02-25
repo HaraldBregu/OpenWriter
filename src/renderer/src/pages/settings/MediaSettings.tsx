a03455cebef7395931d664bcd5332fabd438255c
HaraldBregu <harald.bregu@gmail.com>
2026-02-25 09:23:47 +0100

    Add i18n translations to MediaSettings

--- src/renderer/src/pages/settings/MediaSettings.tsx
+++ src/renderer/src/pages/settings/MediaSettings.tsx
@@ -1,4 +1,5 @@
 import React, { lazy, Suspense } from 'react'
+import { useTranslation } from 'react-i18next'
 import { CollapsibleSection } from './CollapsibleSection'
 import { LoadingSkeleton } from '../../components/LoadingSkeleton'
 
@@ -7,19 +8,21 @@ const CameraPage = lazy(() => import('../CameraPage'))
 const ScreenPage = lazy(() => import('../ScreenPage'))
 
 const MediaSettings: React.FC = () => {
+  const { t } = useTranslation()
+
   return (
     <div className="divide-y">
-      <CollapsibleSection title="Microphone">
+      <CollapsibleSection title={t('settings.sections.microphone')}>
         <Suspense fallback={<LoadingSkeleton />}>
           <MicrophonePage />
         </Suspense>
       </CollapsibleSection>
-      <CollapsibleSection title="Camera">
+      <CollapsibleSection title={t('settings.sections.camera')}>
         <Suspense fallback={<LoadingSkeleton />}>
           <CameraPage />
         </Suspense>
       </CollapsibleSection>
-      <CollapsibleSection title="Screen">
+      <CollapsibleSection title={t('settings.sections.screen')}>
         <Suspense fallback={<LoadingSkeleton />}>
           <ScreenPage />
         </Suspense>
