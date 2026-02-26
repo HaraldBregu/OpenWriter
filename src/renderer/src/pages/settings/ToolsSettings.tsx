c10893b360f37e1fd6729177dfedf0ebb0cbc566
HaraldBregu <harald.bregu@gmail.com>
2026-02-26 16:54:37 +0100

    Remove ToolsSettings sections

--- src/renderer/src/pages/settings/ToolsSettings.tsx
+++ src/renderer/src/pages/settings/ToolsSettings.tsx
@@ -1,46 +1,7 @@
-import React, { lazy, Suspense } from 'react'
-import { useTranslation } from 'react-i18next'
-import { CollapsibleSection } from './CollapsibleSection'
-import { LoadingSkeleton } from '../../components/LoadingSkeleton'
-
-const LifecyclePage = lazy(() => import('../LifecyclePage'))
-const WindowManagerPage = lazy(() => import('../WindowManagerPage'))
-const FilesystemPage = lazy(() => import('../FilesystemPage'))
-const DialogsPage = lazy(() => import('../DialogsPage'))
-const ClipboardPage = lazy(() => import('../ClipboardPage'))
+import React from 'react'
 
 const ToolsSettings: React.FC = () => {
-  const { t } = useTranslation()
-
-  return (
-    <div className="divide-y">
-      <CollapsibleSection title={t('settings.sections.lifecycle')}>
-        <Suspense fallback={<LoadingSkeleton />}>
-          <LifecyclePage />
-        </Suspense>
-      </CollapsibleSection>
-      <CollapsibleSection title={t('settings.sections.windows')}>
-        <Suspense fallback={<LoadingSkeleton />}>
-          <WindowManagerPage />
-        </Suspense>
-      </CollapsibleSection>
-      <CollapsibleSection title={t('settings.sections.filesystem')}>
-        <Suspense fallback={<LoadingSkeleton />}>
-          <FilesystemPage />
-        </Suspense>
-      </CollapsibleSection>
-      <CollapsibleSection title={t('settings.sections.dialogs')}>
-        <Suspense fallback={<LoadingSkeleton />}>
-          <DialogsPage />
-        </Suspense>
-      </CollapsibleSection>
-      <CollapsibleSection title={t('settings.sections.clipboard')}>
-        <Suspense fallback={<LoadingSkeleton />}>
-          <ClipboardPage />
-        </Suspense>
-      </CollapsibleSection>
-    </div>
-  )
+  return null
 }
 
 export default ToolsSettings
