b92de8135795fd81884d868feb1ee76a43fe9f84
harald bregu <harald.bregu@gmail.com>
2026-02-26 08:12:55 +0100

    Remove exhaustive-deps disable from WelcomePage

--- src/renderer/src/pages/WelcomePage.tsx
+++ src/renderer/src/pages/WelcomePage.tsx
@@ -23,7 +23,7 @@ const WelcomePage: React.FC = () => {
 
   useEffect(() => {
     loadRecentProjects()
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps -- intentionally runs once on mount only
+  }, [])
 
   const loadRecentProjects = async () => {
     try {
