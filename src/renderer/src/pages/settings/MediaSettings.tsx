e96e89017c2e5de13afe607c10f7041ef146b3c0
HaraldBregu <harald.bregu@gmail.com>
2026-02-26 16:53:40 +0100

    Remove media settings subsections

--- src/renderer/src/pages/settings/MediaSettings.tsx
+++ src/renderer/src/pages/settings/MediaSettings.tsx
@@ -1,34 +1,7 @@
-import React, { lazy, Suspense } from 'react'
-import { useTranslation } from 'react-i18next'
-import { CollapsibleSection } from './CollapsibleSection'
-import { LoadingSkeleton } from '../../components/LoadingSkeleton'
-
-const MicrophonePage = lazy(() => import('../MicrophonePage'))
-const CameraPage = lazy(() => import('../CameraPage'))
-const ScreenPage = lazy(() => import('../ScreenPage'))
+import React from 'react'
 
 const MediaSettings: React.FC = () => {
-  const { t } = useTranslation()
-
-  return (
-    <div className="divide-y">
-      <CollapsibleSection title={t('settings.sections.microphone')}>
-        <Suspense fallback={<LoadingSkeleton />}>
-          <MicrophonePage />
-        </Suspense>
-      </CollapsibleSection>
-      <CollapsibleSection title={t('settings.sections.camera')}>
-        <Suspense fallback={<LoadingSkeleton />}>
-          <CameraPage />
-        </Suspense>
-      </CollapsibleSection>
-      <CollapsibleSection title={t('settings.sections.screen')}>
-        <Suspense fallback={<LoadingSkeleton />}>
-          <ScreenPage />
-        </Suspense>
-      </CollapsibleSection>
-    </div>
-  )
+  return null
 }
 
 export default MediaSettings
