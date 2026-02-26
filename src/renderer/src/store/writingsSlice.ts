9d5755939a54271a4a9e2d11d2d5989f1eb1accb
harald bregu <harald.bregu@gmail.com>
2026-02-26 21:11:37 +0100

    Export writing creation error actions

--- src/renderer/src/store/writingsSlice.ts
+++ src/renderer/src/store/writingsSlice.ts
@@ -244,6 +244,8 @@ export const {
   deleteWriting,
   loadWritings,
   hydrateWritingsFromDisk,
+  setWritingCreationError,
+  clearWritingCreationError,
 } = writingsSlice.actions
 
 // ---------------------------------------------------------------------------
