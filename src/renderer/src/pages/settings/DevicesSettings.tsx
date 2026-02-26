9a3d7c40109206590187ebca249377acafe2a132
HaraldBregu <harald.bregu@gmail.com>
2026-02-26 16:53:12 +0100

    Remove demo feature pages

--- src/renderer/src/pages/settings/DevicesSettings.tsx
+++ src/renderer/src/pages/settings/DevicesSettings.tsx
@@ -1,28 +1,7 @@
-import React, { lazy, Suspense } from 'react'
-import { useTranslation } from 'react-i18next'
-import { CollapsibleSection } from './CollapsibleSection'
-import { LoadingSkeleton } from '../../components/LoadingSkeleton'
-
-const BluetoothPage = lazy(() => import('../BluetoothPage'))
-const NetworkPage = lazy(() => import('../NetworkPage'))
+import React from 'react'
 
 const DevicesSettings: React.FC = () => {
-  const { t } = useTranslation()
-
-  return (
-    <div className="divide-y">
-      <CollapsibleSection title={t('settings.sections.bluetooth')}>
-        <Suspense fallback={<LoadingSkeleton />}>
-          <BluetoothPage />
-        </Suspense>
-      </CollapsibleSection>
-      <CollapsibleSection title={t('settings.sections.network')}>
-        <Suspense fallback={<LoadingSkeleton />}>
-          <NetworkPage />
-        </Suspense>
-      </CollapsibleSection>
-    </div>
-  )
+  return null
 }
 
 export default DevicesSettings
