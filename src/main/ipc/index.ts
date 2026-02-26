0acfb14ee892e057f34f1da4066c00c4036fca98
harald bregu <harald.bregu@gmail.com>
2026-02-26 23:56:52 +0100

    Export ContextMenuIpc from ipc module

--- src/main/ipc/index.ts
+++ src/main/ipc/index.ts
@@ -5,6 +5,7 @@
 
 export type { IpcModule } from './IpcModule'
 export { AgentIpc } from './AgentIpc'
+export { ContextMenuIpc } from './ContextMenuIpc'
 export { CronIpc } from './CronIpc'
 export { CustomIpc } from './CustomIpc'
 export { DocumentsIpc } from './DocumentsIpc'
