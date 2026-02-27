9a361714f90c3a322484560a1f6342d2ef8d8793
harald bregu <harald.bregu@gmail.com>
2026-02-27 21:46:21 +0100

    Fix PersonalityIpc to use flattened WorkspaceChannels.personalityLoadAll key

--- src/main/ipc/PersonalityIpc.ts
+++ src/main/ipc/PersonalityIpc.ts
@@ -72,7 +72,7 @@ export class PersonalityIpc implements IpcModule {
      * Output: PersonalityFile[] - Array of all personality files
      */
     ipcMain.handle(
-      WorkspaceChannels.personality.loadAll,
+      WorkspaceChannels.personalityLoadAll,
       wrapIpcHandler(async (event: IpcMainInvokeEvent): Promise<PersonalityFile[]> => {
         const personalityFiles = getWindowService<PersonalityFilesService>(event, container, 'personalityFiles')
 
@@ -81,7 +81,7 @@ export class PersonalityIpc implements IpcModule {
         console.log(`[PersonalityIpc] Loaded ${files.length} personality files`)
 
         return files
-      }, WorkspaceChannels.personality.loadAll)
+      }, WorkspaceChannels.personalityLoadAll)
     )
 
     /**
