419acd07ea850f335652feb157c8a129f04c625a
HaraldBregu <harald.bregu@gmail.com>
2026-02-27 16:28:42 +0100

    Add barrel export file for agentManager module

---
+++ src/main/agentManager/index.ts
@@ -0,0 +1,12 @@
+export { AgentManager } from './AgentManager'
+export { AgentManagerIpc } from './AgentManagerIpc'
+export { AgentSession } from './AgentSession'
+export { executeAgentStream } from './AgentExecutor'
+export type {
+  AgentSessionConfig,
+  AgentRequest,
+  AgentStreamEvent,
+  AgentSessionSnapshot,
+  AgentRunSnapshot,
+  AgentManagerStatus,
+} from './AgentManagerTypes'
