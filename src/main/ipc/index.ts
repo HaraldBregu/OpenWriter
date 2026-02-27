c4171ececb6ce5f732c1fd8d2d1c29a597f37228
HaraldBregu <harald.bregu@gmail.com>
2026-02-27 17:25:11 +0100

    Remove unused IPC modules for agent, cron, output, and pipeline

--- src/main/ipc/index.ts
+++ src/main/ipc/index.ts
@@ -4,13 +4,8 @@
  */
 
 export type { IpcModule } from './IpcModule'
-export { AgentIpc } from './AgentIpc'
-export { CronIpc } from './CronIpc'
 export { CustomIpc } from './CustomIpc'
 export { DocumentsIpc } from './DocumentsIpc'
-export { PipelineIpc } from './PipelineIpc'
-// StoreIpc has been merged into CustomIpc. Store handlers are now registered
-// there and exposed to the renderer via window.app instead of window.store.
 export { TaskIpc } from './TaskIpc'
 export { WorkspaceIpc } from './WorkspaceIpc'
 export { DirectoriesIpc } from './DirectoriesIpc'
