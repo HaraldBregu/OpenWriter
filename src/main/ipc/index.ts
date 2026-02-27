7e88d8fdc5baa25ec93a39bc587e3802f10997dd
harald bregu <harald.bregu@gmail.com>
2026-02-28 00:34:07 +0100

    Fix AIAgentsManagerIpc import to use PascalCase module filename

--- src/main/ipc/index.ts
+++ src/main/ipc/index.ts
@@ -5,7 +5,7 @@
 
 export type { IpcModule } from './IpcModule'
 export { AppIpc } from './AppIpc'
-export { AIAgentsManagerIpc } from './aiAgentsManagerIpc'
+export { AIAgentsManagerIpc } from './AIAgentsManagerIpc'
 export { TasksManagerIpc } from './TasksManagerIpc'
 export { WorkspaceIpc } from './WorkspaceIpc'
 export { WindowIpc } from './WindowIpc'
