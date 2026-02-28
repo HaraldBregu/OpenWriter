6b703288779b4a22dedf73865e8805bfdec3dfdd
harald bregu <harald.bregu@gmail.com>
2026-02-28 23:14:49 +0100

    Update Block import to use block.types in useContentEditor

--- src/renderer/src/hooks/useContentEditor.ts
+++ src/renderer/src/hooks/useContentEditor.ts
@@ -1,6 +1,6 @@
 import { useState, useCallback, useEffect, useRef } from 'react'
 import { useAppDispatch, useAppSelector } from '@/store'
-import { createBlock, type Block } from '@/components/ContentBlock'
+import { createBlock, type Block } from '@/components/block.types'
 import {
   selectWritingEntryById,
   setWritingItemId,
