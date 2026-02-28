6231c56077dbec698dd0dd110f6506a27bad16a8
harald bregu <harald.bregu@gmail.com>
2026-02-28 15:45:49 +0100

    Update App.tsx routes to use ContentPage component

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -52,8 +52,8 @@ const App: React.FC = () => {
                       <Routes>
                         <Route path="/home" element={<RouteWrapper><HomePage /></RouteWrapper>} />
                         <Route path="/settings" element={<RouteWrapper><SettingsPage /></RouteWrapper>} />
-                        <Route path="/new/writing" element={<RouteWrapper><NewWritingPage /></RouteWrapper>} />
-                        <Route path="/new/writing/:id" element={<RouteWrapper><NewWritingPage /></RouteWrapper>} />
+                        <Route path="/new/writing" element={<RouteWrapper><ContentPage /></RouteWrapper>} />
+                        <Route path="/new/writing/:id" element={<RouteWrapper><ContentPage /></RouteWrapper>} />
                         <Route path="/documents" element={<RouteWrapper><DocumentsPage /></RouteWrapper>} />
                         <Route path="/directories" element={<RouteWrapper><DirectoriesPage /></RouteWrapper>} />
                         <Route path="/personality/emotional-depth" element={<RouteWrapper><EmotionalDepthPage /></RouteWrapper>} />
