3fca4ca54e04aa836efcb8b0b55aef657871c66a
harald bregu <harald.bregu@gmail.com>
2026-03-01 19:37:32 +0100

    Remove eslint-disable comment for unused import in useContentEditor

--- src/renderer/src/hooks/useContentEditor.ts
+++ src/renderer/src/hooks/useContentEditor.ts
@@ -1,5 +1,4 @@
 import { useState, useCallback, useEffect, useRef } from 'react'
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { useAppDispatch, useAppSelector } from '@/store'
 import { createBlock, type Block } from '@/components/block.types'
 import {
