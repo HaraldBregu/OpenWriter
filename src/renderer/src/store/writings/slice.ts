62b71c01f0dbd54bde94b8359e74ff03df04c335
harald bregu <harald.bregu@gmail.com>
2026-03-02 21:41:34 +0100

    Add writings store compatibility facade slice

---
+++ src/renderer/src/store/writings/slice.ts
@@ -0,0 +1,32 @@
+/**
+ * @deprecated This file is a compatibility facade.
+ * Import from the split files directly:
+ *   - Types:     '@/store/writings/types'
+ *   - Actions:   '@/store/writings/actions'
+ *   - Selectors: '@/store/writings/selectors'
+ *   - Reducer:   '@/store/writings/reducer'
+ *   - Barrel:    '@/store/writings'
+ */
+
+export type { WritingItem, WritingsState } from './types'
+
+export {
+  writingsSlice,
+  writingsLoaded,
+  writingAdded,
+  writingUpdated,
+  writingRemoved,
+  writingSelected,
+  writingsLoadingStarted,
+  writingsLoadingFailed,
+} from './reducer'
+export { default } from './reducer'
+
+export {
+  selectAllWritings,
+  selectSelectedWriting,
+  selectSelectedWritingId,
+  selectWritingsStatus,
+  selectWritingsError,
+  selectWritingById,
+} from './selectors'
