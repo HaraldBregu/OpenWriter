79c0b4ab04cdf9e870b2cb66bb7748a925a0f352
HaraldBregu <harald.bregu@gmail.com>
2026-03-03 17:05:12 +0100

    Add BubbleMenu TipTap extension wrapper for bubble-menu-plugin

---
+++ src/renderer/src/components/app/editor/bubble-menu/bubble-menu-extension.ts
@@ -0,0 +1,38 @@
+import { Extension } from '@tiptap/core'
+import { PluginKey } from '@tiptap/pm/state'
+import { BubbleMenuPlugin, type BubbleMenuPluginProps } from './bubble-menu-plugin'
+
+export type BubbleMenuExtensionOptions = Omit<BubbleMenuPluginProps, 'editor' | 'element'> & {
+  element: HTMLElement | null
+}
+
+export const BubbleMenuExtension = Extension.create<BubbleMenuExtensionOptions>({
+  name: 'bubbleMenu',
+
+  addOptions() {
+    return {
+      element: null,
+      pluginKey: 'bubbleMenu',
+      updateDelay: 250,
+      shouldShow: null,
+    }
+  },
+
+  addProseMirrorPlugins() {
+    if (!this.options.element) {
+      return []
+    }
+
+    return [
+      BubbleMenuPlugin({
+        pluginKey: this.options.pluginKey instanceof PluginKey
+          ? this.options.pluginKey
+          : new PluginKey(this.options.pluginKey),
+        editor: this.editor,
+        element: this.options.element,
+        updateDelay: this.options.updateDelay,
+        shouldShow: this.options.shouldShow,
+      }),
+    ]
+  },
+})
