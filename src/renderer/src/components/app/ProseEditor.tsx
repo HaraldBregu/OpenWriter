b9d268a05fea5180fc70952aec2f51d9076d0f28
HaraldBregu <harald.bregu@gmail.com>
2026-03-03 09:16:25 +0100

    Replace MutableRefObject with RefObject in ref callback cast

--- src/renderer/src/components/app/ProseEditor.tsx
+++ src/renderer/src/components/app/ProseEditor.tsx
@@ -1023,7 +1023,7 @@ const ProseEditor = memo(
         if (typeof ref === 'function') {
           ref(node)
         } else if (ref) {
-          ;(ref as React.MutableRefObject<HTMLDivElement | null>).current = node
+          ;(ref as React.RefObject<HTMLDivElement | null>).current = node
         }
       },
       [ref],
