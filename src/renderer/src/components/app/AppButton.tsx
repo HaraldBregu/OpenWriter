11e5aa829d422324c109facd292c807e871def85
harald bregu <harald.bregu@gmail.com>
2026-03-03 22:17:59 +0100

    Add icon size variants to AppButton using cva

--- src/renderer/src/components/app/AppButton.tsx
+++ src/renderer/src/components/app/AppButton.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
-import { Button, buttonVariants, type ButtonProps } from '../ui/button'
+import { cva } from 'class-variance-authority'
+import { Button, buttonVariants as baseButtonVariants, type ButtonProps } from '../ui/button'
 import { cn } from '@/lib/utils'
+
+const buttonVariants = cva(baseButtonVariants(), {
+  variants: {
+    size: {
+      'icon-lg': 'h-10 w-10 rounded-full p-0 [&_svg]:size-5',
+      icon: 'h-8 w-8 rounded-full p-0 [&_svg]:size-4',
+      'icon-sm': 'h-7 w-7 rounded-full p-0 [&_svg]:size-[17px]',
+      'icon-xm': 'h-6 w-6 rounded p-0 [&_svg]:size-[15px]',
+    },
+  },
+})
 
 const AppButton = React.memo(
   React.forwardRef<React.ElementRef<typeof Button>, React.ComponentPropsWithoutRef<typeof Button>>(
