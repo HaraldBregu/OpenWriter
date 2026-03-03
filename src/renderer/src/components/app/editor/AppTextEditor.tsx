ecaeb6bd910cd21321d86876114cc7fc99ff69d0
HaraldBregu <harald.bregu@gmail.com>
2026-03-03 12:39:03 +0100

    Add placeholder prop to TipTapAdapterProps interface

--- src/renderer/src/components/app/editor/AppTextEditor.tsx
+++ src/renderer/src/components/app/editor/AppTextEditor.tsx
@@ -217,6 +217,7 @@ interface TipTapAdapterProps {
   onAddBelow: ((pos: number) => void) | undefined
   onDelete: ((pos: number) => void) | undefined
   onEnhance: ((pos: number) => void) | undefined
+  placeholder: string | undefined
 }
 
 function TipTapAdapter({
