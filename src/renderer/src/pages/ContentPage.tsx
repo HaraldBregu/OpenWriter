cddb79515ba5ad86452f58ea61c53da577e40262
HaraldBregu <harald.bregu@gmail.com>
2026-03-04 14:59:38 +0100

    Add max-width constraint and center content page layout

--- src/renderer/src/pages/ContentPage.tsx
+++ src/renderer/src/pages/ContentPage.tsx
@@ -245,7 +245,7 @@ const ContentPage: React.FC = () => {
       </div>
 
       <div className="flex-1 overflow-y-auto overflow-x-hidden bg-background">
-        <div className="w-full px-10 py-10 flex flex-col gap-2">
+        <div className="w-full max-w-4xl mx-auto px-10 py-10 flex flex-col gap-2">
           <TextEditor
             value={content}
             onChange={handleContentChange}
