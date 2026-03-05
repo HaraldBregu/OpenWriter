db485b8a87b3c158b7f860d304363430e2032930
HaraldBregu <harald.bregu@gmail.com>
2026-03-05 15:49:39 +0100

    Change default editor zoom value from 200 to 150

--- src/renderer/src/components/editor/TextEditor.tsx
+++ src/renderer/src/components/editor/TextEditor.tsx
@@ -245,7 +245,7 @@ const TextEditor = React.memo(
                 </EditorProvider>
               )}
               <EditorContent 
-                style={{ 'zoom': 200 / 100 }}
+                style={{ 'zoom': 150 / 100 }}
 
               editor={editor} />
             </div>
