9d5eb4062e141c5f8cba5def251118ba8552ab06
HaraldBregu <harald.bregu@gmail.com>
2026-03-05 15:10:16 +0100

    Replace console.log with LoggerService in DocumentsWatcherService watch start

--- src/main/services/documents-watcher.ts
+++ src/main/services/documents-watcher.ts
@@ -140,7 +140,7 @@ export class DocumentsWatcherService implements Disposable {
       return
     }
 
-    console.log('[DocumentsWatcherService] Starting to watch:', docsDir)
+    this.logger?.info('DocumentsWatcherService', `Starting to watch: ${docsDir}`)
 
     try {
       this.watcher = chokidar.watch(docsDir, {
