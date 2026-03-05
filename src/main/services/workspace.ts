509993fe5da61f5b83a6bd6e2a783a50289c657d
HaraldBregu <harald.bregu@gmail.com>
2026-03-05 15:47:23 +0100

    Add LoggerService type import to workspace service

--- src/main/services/workspace.ts
+++ src/main/services/workspace.ts
@@ -3,6 +3,7 @@ import path from 'node:path'
 import type { StoreService } from './store'
 import type { EventBus } from '../core/EventBus'
 import type { Disposable } from '../core/ServiceContainer'
+import type { LoggerService } from './logger'
 import { WORKSPACE_VALIDATION_INTERVAL_MS } from '../constants'
 
 /**
