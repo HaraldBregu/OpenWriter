e80a8975463a75ac23fb952eb92df3719cc9f97b
HaraldBregu <harald.bregu@gmail.com>
2026-03-06 11:24:06 +0100

    Add ModelRegistry import to bootstrap module

--- src/main/bootstrap.ts
+++ src/main/bootstrap.ts
@@ -19,7 +19,7 @@ import { ServiceContainer, EventBus, WindowFactory, AppState, WindowContextManag
 import { StoreService } from './services/store';
 import { LoggerService } from './services/logger';
 import { FileManagementService } from './services/FileManagementService';
-import { AgentRegistry, ALL_AGENT_DEFINITIONS } from './agents';
+import { AgentRegistry, ModelRegistry, ALL_AGENT_DEFINITIONS } from './agents';
 import { TaskHandlerRegistry } from './taskManager/TaskHandlerRegistry';
 import { TaskExecutor } from './taskManager/TaskExecutor';
 import { TaskReactionRegistry } from './taskManager/TaskReactionRegistry';
