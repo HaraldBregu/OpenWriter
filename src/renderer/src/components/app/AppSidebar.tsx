f5b35f42c3e3fae9d651c15c1ee5892932cff949
HaraldBregu <harald.bregu@gmail.com>
2026-03-06 14:14:51 +0100

    Update imports to kebab-case paths in AppSidebar

--- src/renderer/src/components/app/AppSidebar.tsx
+++ src/renderer/src/components/app/AppSidebar.tsx
@@ -24,7 +24,7 @@ import {
 	SidebarSeparator,
 	SidebarTrigger,
 	useSidebar,
-} from '../ui/sidebar';
+} from '../ui/Sidebar';
 import { cn } from '@/lib/utils';
 
 const AppSidebarProvider = React.memo(
