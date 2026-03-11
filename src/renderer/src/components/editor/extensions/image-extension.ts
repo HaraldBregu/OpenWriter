3833f407603997f175a90b9a58c2b914c820b8ab
HaraldBregu <harald.bregu@gmail.com>
2026-03-11 15:32:58 +0100

    Add image extension for editor with drop/paste support

---
+++ src/renderer/src/components/editor/extensions/image-extension.ts
@@ -0,0 +1,68 @@
+import { Node, mergeAttributes } from '@tiptap/core';
+import { ReactNodeViewRenderer } from '@tiptap/react';
+import { ImageNodeView } from './views/ImageNodeView';
+import { createImageDropPastePlugin, fileToDataUri, type ImageFileHandler } from './image-drop-paste-plugin';
+
+export interface ImageExtensionOptions {
+	/**
+	 * Called for every image File that is dropped or pasted into the editor.
+	 * Must return a Promise resolving to the image src string (data URI or path).
+	 * Defaults to converting the file to a data URI.
+	 */
+	onImageFile: ImageFileHandler;
+}
+
+declare module '@tiptap/core' {
+	interface Commands<ReturnType> {
+		image: {
+			setImage: (attrs: { src: string; alt?: string; title?: string }) => ReturnType;
+		};
+	}
+}
+
+export const ImageExtension = Node.create<ImageExtensionOptions>({
+	name: 'image',
+	group: 'block',
+	atom: true,
+	draggable: true,
+
+	addOptions() {
+		return {
+			onImageFile: fileToDataUri,
+		};
+	},
+
+	addAttributes() {
+		return {
+			src: { default: null },
+			alt: { default: null },
+			title: { default: null },
+		};
+	},
+
+	parseHTML() {
+		return [{ tag: 'img[src]' }];
+	},
+
+	renderHTML({ HTMLAttributes }) {
+		return ['img', mergeAttributes(HTMLAttributes)];
+	},
+
+	addCommands() {
+		return {
+			setImage:
+				(attrs) =>
+				({ commands }) => {
+					return commands.insertContent({ type: this.name, attrs });
+				},
+		};
+	},
+
+	addProseMirrorPlugins() {
+		return [createImageDropPastePlugin(this.options.onImageFile)];
+	},
+
+	addNodeView() {
+		return ReactNodeViewRenderer(ImageNodeView);
+	},
+});
