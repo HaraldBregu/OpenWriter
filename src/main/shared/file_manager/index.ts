822a589077f36c4469c1d52ca92512dbbea7af28
HaraldBregu <harald.bregu@gmail.com>
2026-03-11 13:13:55 +0100

    Add file manager barrel export for types and FileSystemManager

---
+++ src/main/shared/file_manager/index.ts
@@ -0,0 +1,10 @@
+export type {
+	FileEncoding,
+	ReadFileOptions,
+	WriteFileOptions,
+	CreateFileOptions,
+	CreateFolderOptions,
+	RenameOptions,
+	RenameResult,
+} from './types';
+export { FileSystemManager } from './file-system-manager';
