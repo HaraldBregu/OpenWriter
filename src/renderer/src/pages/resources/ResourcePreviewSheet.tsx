9ab5f905cf51e50647c7273c7a046e98469b1454
harald bregu <harald.bregu@gmail.com>
2026-03-12 23:10:03 +0100

    Add useTranslation import to ResourcePreviewSheet

--- src/renderer/src/pages/resources/ResourcePreviewSheet.tsx
+++ src/renderer/src/pages/resources/ResourcePreviewSheet.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { FileWarning, Loader2 } from 'lucide-react';
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
