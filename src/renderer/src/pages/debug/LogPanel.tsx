dc66f84def75fe80ff58d54891d903a0cfce1ae5
harald bregu <harald.bregu@gmail.com>
2026-03-12 23:17:05 +0100

    Add useTranslation import to LogPanel component

--- src/renderer/src/pages/debug/LogPanel.tsx
+++ src/renderer/src/pages/debug/LogPanel.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { X } from 'lucide-react';
 import type { TrackedTaskState } from '@/store/tasks/types';
 import { formatEventTime } from './debug-helpers';
