895aeae6103ba0c5e00be3c58109b44f0fab8d2a
harald bregu <harald.bregu@gmail.com>
2026-03-12 23:10:58 +0100

    Initialize useTranslation hook in BlockActions component

--- src/renderer/src/components/editor/BlockActions.tsx
+++ src/renderer/src/components/editor/BlockActions.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Copy, Trash2, Clipboard, MoreVertical } from 'lucide-react';
 import { useEditorContext } from './EditorContext';
 import { cn } from '@/lib/utils';
