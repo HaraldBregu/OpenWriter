afa47a0ebf2680164c2025bda8e279972222ec32
harald bregu <harald.bregu@gmail.com>
2026-03-14 17:15:12 +0100

    Add cn utility import to AppLayout component

--- src/renderer/src/components/AppLayout.tsx
+++ src/renderer/src/components/AppLayout.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { cn } from '@/lib/utils';
 import { useTranslation } from 'react-i18next';
 import { useLanguage } from '../hooks/use-language';
 import { useWorkspaceListener } from '../hooks/use-workspace-listener';
