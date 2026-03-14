5667c07afadbcc0ec2d6576826fe9e6384c61ca0
harald bregu <harald.bregu@gmail.com>
2026-03-14 19:17:43 +0100

    Add metadata field to task reducer initial state

--- src/renderer/src/store/tasks/reducer.ts
+++ src/renderer/src/store/tasks/reducer.ts
@@ -97,6 +97,7 @@ export const tasksSlice = createSlice({
 						status: 'queued',
 						priority: 'normal',
 						progress: { percent: 0 },
+						metadata: event.data.metadata,
 						streamBuffer: '',
 						events: [],
 					};
