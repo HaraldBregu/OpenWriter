8af032f4c9a48427ee69b40b990c788f0882d9ee
harald bregu <harald.bregu@gmail.com>
2026-03-14 22:17:04 +0100

    Rename TextWriterAgent export to TextCompleterAgent in agents index

--- src/main/ai/agents/index.ts
+++ src/main/ai/agents/index.ts
@@ -1,2 +1,2 @@
-export { TextWriterAgent } from './text_writer/definition';
+export { TextCompleterAgent } from './text_completer/definition';
 export { TextEnhanceAgent } from './text_enhance/definition';
