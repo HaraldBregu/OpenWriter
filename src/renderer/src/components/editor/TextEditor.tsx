c0533107855cc3e052fd899caadbc3aa1879e562
harald bregu <harald.bregu@gmail.com>
2026-03-15 18:09:18 +0100

    Remove basePath and documentPath dependency from extension handlers

--- src/renderer/src/components/editor/TextEditor.tsx
+++ src/renderer/src/components/editor/TextEditor.tsx
@@ -108,9 +108,8 @@ const TextEditor = React.memo(
 							onAgentPromptSubmitRef.current?.(before, after, cursorPos, prompt),
 						onImagePlaceholderSubmit: (prompt) => onImagePlaceholderSubmitRef.current?.(prompt),
 						onImagePlaceholderFileSelect: (file) => onImagePlaceholderFileSelectRef.current?.(file),
-						basePath: documentPath ?? null,
 					}),
-				[documentPath]
+				[]
 			);
 
 			const lastEmittedRef = useRef<string>('');
