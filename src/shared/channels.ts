9b5b9ee69c4ebe7de51e076dfe69f4d7ed680087
HaraldBregu <harald.bregu@gmail.com>
2026-03-16 15:19:23 +0100

    Add getDocumentPath channel to workspace channels

--- src/shared/channels.ts
+++ src/shared/channels.ts
@@ -79,6 +79,7 @@ export const WorkspaceChannels = {
 	openDataFolder: 'workspace:open-data-folder',
 	openResourcesFolder: 'workspace:open-resources-folder',
 	openDocumentFolder: 'workspace:open-document-folder',
+	getDocumentPath: 'workspace:get-document-path',
 	// Document images
 	saveDocumentImage: 'output:save-document-image',
 	// Output
