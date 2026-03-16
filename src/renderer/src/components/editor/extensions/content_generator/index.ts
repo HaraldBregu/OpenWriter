3495f81c208b5ece5cab1e37e6dd5c1745c44c09
HaraldBregu <harald.bregu@gmail.com>
2026-03-16 11:36:21 +0100

    Add barrel export file for content generator extension

---
+++ src/renderer/src/components/editor/extensions/content_generator/index.ts
@@ -0,0 +1,3 @@
+export { ContentGeneratorNodeView } from './NodeView';
+export { ContentGeneratorExtension } from './input-extension';
+export type { ContentGeneratorOptions, ContentGeneratorMode } from './input-extension';
