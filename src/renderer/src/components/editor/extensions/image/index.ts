ca2070d4c11b8127fd8a8f90cbff98c1735fff3b
HaraldBregu <harald.bregu@gmail.com>
2026-03-17 09:14:02 +0100

    Rename exported type ImageFileHandler to ImageInsertHandler

--- src/renderer/src/components/editor/extensions/image/index.ts
+++ src/renderer/src/components/editor/extensions/image/index.ts
@@ -2,5 +2,5 @@ export { ImageNodeView } from './NodeView';
 export { ImageExtension } from './input-extension';
 export type { ImageExtensionOptions } from './input-extension';
 export { createImageDropPastePlugin, fileToDataUri } from './image-drop-paste-plugin';
-export type { ImageFileHandler } from './image-drop-paste-plugin';
+export type { ImageInsertHandler } from './image-drop-paste-plugin';
 export { ImageEditor } from './ImageEditor';
