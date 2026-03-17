18c96e155f58fbe06ee4f8dd77692e8bb8bccb9c
HaraldBregu <harald.bregu@gmail.com>
2026-03-17 08:47:35 +0100

    Add ImageEditor export to image extension barrel file

--- src/renderer/src/components/editor/extensions/image/index.ts
+++ src/renderer/src/components/editor/extensions/image/index.ts
@@ -3,3 +3,4 @@ export { ImageExtension } from './input-extension';
 export type { ImageExtensionOptions } from './input-extension';
 export { createImageDropPastePlugin, fileToDataUri } from './image-drop-paste-plugin';
 export type { ImageFileHandler } from './image-drop-paste-plugin';
+export { ImageEditor } from './ImageEditor';
