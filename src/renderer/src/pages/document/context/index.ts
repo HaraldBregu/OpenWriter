37129128d83b2f29493d18dbacd2958bb27139d6
HaraldBregu <harald.bregu@gmail.com>
2026-03-17 15:03:42 +0100

    Add barrel export for document context module

---
+++ src/renderer/src/pages/document/context/index.ts
@@ -0,0 +1,5 @@
+export { DocumentProvider, DocumentStateContext, DocumentDispatchContext } from './DocumentContext';
+export type { DocumentState } from './state';
+export { INITIAL_DOCUMENT_STATE } from './state';
+export type { DocumentAction } from './actions';
+export { documentReducer } from './reducer';
