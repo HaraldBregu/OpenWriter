d985875dc20e82a3d0740aa9b9d1807cf7b31761
HaraldBregu <harald.bregu@gmail.com>
2026-03-17 15:04:02 +0100

    Add barrel export for document hooks module

---
+++ src/renderer/src/pages/document/hooks/index.ts
@@ -0,0 +1,5 @@
+export { useDocumentState } from './use-document-state';
+export { useDocumentDispatch } from './use-document-dispatch';
+export { useDocumentUI } from './use-document-ui';
+export { useDocumentActions } from './use-document-actions';
+export { useDocumentPersistence } from './use-document-persistence';
