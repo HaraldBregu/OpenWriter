ebf61c7e0a5faaa321656bd7f7c26a60e08db18d
HaraldBregu <harald.bregu@gmail.com>
2026-03-17 15:44:01 +0100

    Remove unused useState and useEffect imports from ConfigSidebar

--- src/renderer/src/pages/document/ConfigSidebar.tsx
+++ src/renderer/src/pages/document/ConfigSidebar.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState, useEffect, useCallback } from 'react';
+import { useMemo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
 	FileText,
