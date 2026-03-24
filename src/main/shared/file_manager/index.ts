347ed1c8b77aa26f9347fec891ef08783ddb61b4
harald bregu <harald.bregu@gmail.com>
2026-03-24 23:27:28 +0100

    Add file_manager public API exports via index.ts

---
+++ src/main/shared/file_manager/index.ts
@@ -0,0 +1,11 @@
+export type {
+	FileEncoding,
+	FileMetadata,
+	ReadFileOptions,
+	WriteFileOptions,
+	CreateFileOptions,
+	CreateFolderOptions,
+	RenameOptions,
+	RenameResult,
+} from './types';
+export { FileManager } from './file-manager';
