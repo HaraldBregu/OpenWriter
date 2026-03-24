6b0c4540e4f159192310c6f4e077fc33b33e1da4
harald bregu <harald.bregu@gmail.com>
2026-03-24 22:33:40 +0100

    Export SidebarVisibilityContext types from context barrel

--- src/renderer/src/pages/document/context/index.ts
+++ src/renderer/src/pages/document/context/index.ts
@@ -4,3 +4,8 @@ export { INITIAL_DOCUMENT_STATE } from './state';
 export type { DocumentAction } from './actions';
 export { documentReducer } from './reducer';
 export { EditorInstanceProvider, useEditorInstance } from './editor-instance-context';
+export {
+	SidebarVisibilityProvider,
+	useSidebarVisibility,
+} from './sidebar-visibility-context';
+export type { ActiveSidebar } from './sidebar-visibility-context';
