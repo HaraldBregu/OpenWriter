2faa2d7749c07474932e82ca5b5a291acd9c804b
HaraldBregu <harald.bregu@gmail.com>
2026-03-26 10:52:06 +0100

    Remove ResizablePanel wrapper from EditorResizablePanel

--- src/renderer/src/pages/document/EditorResizablePanel.tsx
+++ src/renderer/src/pages/document/EditorResizablePanel.tsx
@@ -38,29 +38,27 @@ const EditorResizablePanel: React.FC<EditorResizablePanelProps> = ({
 	onImageSubmit,
 }) => {
 	return (
-		<ResizablePanel defaultSize="70%" minSize="40%">
-			<div className="h-full flex flex-col min-w-0">
-				<div className="flex-1 overflow-y-auto overflow-x-hidden bg-background">
-					<div className="w-full max-w-4xl mx-auto px-10 py-10 flex flex-col gap-2">
-						{loaded && (
-							<TextEditor
-								disabled={disabled}
-								ref={editorRef}
-								key={documentId}
-								value={content}
-								onChange={onContentChange}
-								onContinueWithAssistant={onContinueWithAssistant}
-								onEnhanceWithAssistant={onEnhanceWithAssistant}
-								onTextSubmit={onTextSubmit}
-								onImageSubmit={onImageSubmit}
-								documentId={documentId}
-								onEditorReady={onEditorReady}
-							/>
-						)}
-					</div>
+		<div className="h-full flex flex-col min-w-0">
+			<div className="flex-1 overflow-y-auto overflow-x-hidden bg-background">
+				<div className="w-full max-w-4xl mx-auto px-10 py-10 flex flex-col gap-2">
+					{loaded && (
+						<TextEditor
+							disabled={disabled}
+							ref={editorRef}
+							key={documentId}
+							value={content}
+							onChange={onContentChange}
+							onContinueWithAssistant={onContinueWithAssistant}
+							onEnhanceWithAssistant={onEnhanceWithAssistant}
+							onTextSubmit={onTextSubmit}
+							onImageSubmit={onImageSubmit}
+							documentId={documentId}
+							onEditorReady={onEditorReady}
+						/>
+					)}
 				</div>
 			</div>
-		</ResizablePanel>
+		</div>
 	);
 };
 
