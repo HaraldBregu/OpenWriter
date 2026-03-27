6da04d951d950bdb6f162f8d1ba9940acf561d98
HaraldBregu <harald.bregu@gmail.com>
2026-03-27 12:48:33 +0100

    update store import to use provider-defaults

--- src/main/services/store.ts
+++ src/main/services/store.ts
@@ -1,6 +1,6 @@
 import Store from 'electron-store';
 import { MAX_RECENT_WORKSPACES } from '../constants';
-import { toProviderConfig, type ServiceProvider } from '../../shared/model-defaults';
+import { toProviderConfig, type ServiceProvider } from '../../shared/provider-defaults';
 
 export interface WorkspaceInfo {
 	path: string;
