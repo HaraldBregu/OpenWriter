2dd44b98233e5cd015e615ef8ca25b8cb8f48371
HaraldBregu <harald.bregu@gmail.com>
2026-03-27 12:48:19 +0100

    update validators import to use provider-defaults

--- src/main/shared/validators.ts
+++ src/main/shared/validators.ts
@@ -3,7 +3,7 @@
  * Used by AppIpc (store handlers) to validate user inputs.
  */
 
-import type { ServiceProvider } from '../../shared/model-defaults';
+import type { ServiceProvider } from '../../shared/provider-defaults';
 
 export class StoreValidators {
 	private static readonly MAX_TOKEN_LENGTH = 500;
