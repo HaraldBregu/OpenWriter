766bd4cacde044597cfb28bec6b6eb7240cc6228
HaraldBregu <harald.bregu@gmail.com>
2026-04-01 15:23:54 +0200

    add AppLogEntry type export to preload type definitions

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -94,6 +94,7 @@ export type {
 	DocumentImageChangeEvent,
 	ProjectWorkspaceInfo,
 	ServiceProvider,
+	AppLogEntry,
 };
 
 // ---------------------------------------------------------------------------
