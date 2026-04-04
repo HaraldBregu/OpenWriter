1eb7ab18648427e8a55e52e0eeaf9aefef66bb7e
harald bregu <harald.bregu@gmail.com>
2026-04-04 23:40:08 +0200

    auto: add barrel export for rag pipeline module

---
+++ src/main/rag/pipeline/index.ts
@@ -0,0 +1,10 @@
+export {
+	VectorIndexingPipeline,
+	type VectorIndexingDocument,
+	type VectorIndexingPhase,
+	type VectorIndexingProgressEvent,
+	type VectorIndexingResult,
+	type RunVectorIndexingInput,
+	type VectorIndexingPipelineOptions,
+} from './indexing-pipeline';
+export { RagManifest, type ManifestEntry } from './rag-manifest';
