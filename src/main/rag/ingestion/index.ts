db66f254035cdc76f6d63361439970d6af9b8731
harald bregu <harald.bregu@gmail.com>
2026-04-04 23:38:13 +0200

    auto: add barrel export for rag ingestion pipeline

---
+++ src/main/rag/ingestion/index.ts
@@ -0,0 +1,4 @@
+export { type DocumentExtractor, type ExtractedContent } from './document-extractor';
+export { ExtractorRegistry } from './extractor-registry';
+export { PlainTextExtractor } from './extractors/plain-text-extractor';
+export { chunkText, type ChunkOptions } from './text-chunker';
