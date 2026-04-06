2f8ee3da0b79bba52575540f6fe63341973c8c6d
harald bregu <harald.bregu@gmail.com>
2026-04-06 02:56:00 +0200

    Export image placeholder extension

---
+++ src/renderer/src/components/editor/extensions/image_placeholder/index.ts
@@ -0,0 +1,3 @@
+export { ImagePlaceholderNodeView } from './NodeView';
+export { ImagePlaceholderExtension } from './input-extension';
+export type { ImagePlaceholderOptions } from './input-extension';
