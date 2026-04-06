02e418e08a466e06a174e8676c12bf62302710d8
harald bregu <harald.bregu@gmail.com>
2026-04-06 12:41:33 +0200

    Import AI filter utilities in image canvas

--- src/renderer/src/components/editor/extensions/image/use-image-canvas.ts
+++ src/renderer/src/components/editor/extensions/image/use-image-canvas.ts
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
+import { detectFiltersFromPrompt, applyFiltersToCanvas } from './ai-filters';
 
 // ---------------------------------------------------------------------------
 // Constants
