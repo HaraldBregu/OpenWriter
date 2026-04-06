6ded536b87b39f8b32a9fca1f1bad6565faaa7c3
harald bregu <harald.bregu@gmail.com>
2026-04-06 02:55:45 +0200

    Add image placeholder extension

---
+++ src/renderer/src/components/editor/extensions/image_placeholder/input-extension.ts
@@ -0,0 +1,54 @@
+import { Node, mergeAttributes } from '@tiptap/core';
+import { ReactNodeViewRenderer } from '@tiptap/react';
+import { ImagePlaceholderNodeView } from './NodeView';
+import type { ImageInsertHandler } from '../image';
+
+export interface ImagePlaceholderOptions {
+	onImageInsert?: ImageInsertHandler;
+}
+
+declare module '@tiptap/core' {
+	interface Commands<ReturnType> {
+		imagePlaceholder: {
+			insertImagePlaceholder: () => ReturnType;
+		};
+	}
+}
+
+export const ImagePlaceholderExtension = Node.create<ImagePlaceholderOptions>({
+	name: 'imagePlaceholder',
+
+	group: 'block',
+
+	atom: true,
+
+	selectable: false,
+
+	draggable: false,
+
+	addOptions() {
+		return {};
+	},
+
+	parseHTML() {
+		return [{ tag: 'div[data-type="image-placeholder"]' }];
+	},
+
+	renderHTML({ HTMLAttributes }) {
+		return ['div', mergeAttributes(HTMLAttributes, { 'data-type': 'image-placeholder' })];
+	},
+
+	addCommands() {
+		return {
+			insertImagePlaceholder:
+				() =>
+				({ commands }) => {
+					return commands.insertContent({ type: this.name });
+				},
+		};
+	},
+
+	addNodeView() {
+		return ReactNodeViewRenderer(ImagePlaceholderNodeView);
+	},
+});
