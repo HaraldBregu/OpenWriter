1ac537463f1abe74175dc4a6c14b799826d35265
HaraldBregu <harald.bregu@gmail.com>
2026-04-07 15:05:09 +0200

    fix: expand inset shorthand to explicit top/right/bottom/left in PDFViewer

--- src/renderer/src/pages/document/panels/resources/components/PdfPreviewDialog.tsx
+++ src/renderer/src/pages/document/panels/resources/components/PdfPreviewDialog.tsx
@@ -58,7 +58,7 @@ export function PdfPreviewDialog({
 							width="100%"
 							height="100%"
 							showToolbar={false}
-							style={{ position: 'absolute', inset: 0 }}
+							style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0 }}
 						>
 							<DocumentPdfTemplate title={title} content={content} metadata={metadata} />
 						</PDFViewer>
