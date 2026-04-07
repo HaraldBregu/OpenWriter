d90be9b4558a1ef0a1a5621890fd93be7b0c01f3
harald bregu <harald.bregu@gmail.com>
2026-04-07 02:10:40 +0200

    auto: conditionally render navigation buttons in TitleBar

--- src/renderer/src/components/TitleBar.tsx
+++ src/renderer/src/components/TitleBar.tsx
@@ -108,23 +108,27 @@ export const TitleBar = React.memo(function TitleBar({
 						</button>
 					)}
 
-					<button
-						type="button"
-						onClick={onNavigateBack}
-						className={btnNoHover}
-						title={t('titleBar.navigateBack')}
-					>
-						<ChevronLeft className="h-[18px] w-[18px]" strokeWidth={1.5} />
-					</button>
+					{onNavigateBack && (
+						<button
+							type="button"
+							onClick={onNavigateBack}
+							className={btnNoHover}
+							title={t('titleBar.navigateBack')}
+						>
+							<ChevronLeft className="h-[18px] w-[18px]" strokeWidth={1.5} />
+						</button>
+					)}
 
-					<button
-						type="button"
-						onClick={onNavigateForward}
-						className={btnNoHover}
-						title={t('titleBar.navigateForward')}
-					>
-						<ChevronRight className="h-[18px] w-[18px]" strokeWidth={1.5} />
-					</button>
+					{onNavigateForward && (
+						<button
+							type="button"
+							onClick={onNavigateForward}
+							className={btnNoHover}
+							title={t('titleBar.navigateForward')}
+						>
+							<ChevronRight className="h-[18px] w-[18px]" strokeWidth={1.5} />
+						</button>
+					)}
 				</div>
 			) : (
 				<div
