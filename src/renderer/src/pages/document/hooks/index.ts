139f9450e47043b748e043bbe0293a8039e84720
harald bregu <harald.bregu@gmail.com>
2026-04-08 01:17:48 +0200

    feat: export useWriterTaskSubmit hook from document hooks index

--- src/renderer/src/pages/document/hooks/index.ts
+++ src/renderer/src/pages/document/hooks/index.ts
@@ -5,3 +5,4 @@ export { useDocumentActions } from './use-document-actions';
 export { useDocumentPersistence } from './use-document-persistence';
 export { useDocumentHistory, type HistoryEntry } from './use-document-history';
 export { useImageTaskSubmit } from './use-image-task-submit';
+export { useWriterTaskSubmit } from './use-writer-task-submit';
