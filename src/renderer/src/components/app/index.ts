c37d5406f320be7881f7e08464e5b1cd91028acc
harald bregu <harald.bregu@gmail.com>
2026-04-09 00:40:21 +0200

    auto: export AppSidebarContainer and AppSidebarInsetLayout

--- src/renderer/src/components/app/index.ts
+++ src/renderer/src/components/app/index.ts
@@ -127,7 +127,9 @@ export { AppSwitch } from './AppSwitch';
 
 export { AppEditorLayout } from './document/AppEditorLayout';
 
+export { AppSidebarContainer } from './sidebar/AppSidebarContainer';
 export { AppSidebarLayout } from './sidebar/AppSidebarLayout';
+export { AppSidebarInsetLayout } from './sidebar/AppSidebarInsetLayout';
 
 export { AppIconOpenWriter } from './icons/AppIconOpenWriter';
 
