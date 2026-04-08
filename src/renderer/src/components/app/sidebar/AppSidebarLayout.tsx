2435e87bfc5b68661e82dd3765aa615c1bedac51
harald bregu <harald.bregu@gmail.com>
2026-04-09 00:39:53 +0200

    auto: simplify AppSidebarLayout to wrap AppSidebar directly

--- src/renderer/src/components/app/sidebar/AppSidebarLayout.tsx
+++ src/renderer/src/components/app/sidebar/AppSidebarLayout.tsx
@@ -1,23 +1,16 @@
 import { memo, type ReactNode, type ReactElement } from 'react';
-import { AppSidebar, AppSidebarInset } from '../AppSidebar';
+import { AppSidebar } from '../AppSidebar';
 
 interface AppSidebarLayoutProps {
-	readonly sidebar: ReactNode;
 	readonly children: ReactNode;
 }
 
 export const AppSidebarLayout = memo(function AppSidebarLayout({
-	sidebar,
 	children,
 }: AppSidebarLayoutProps): ReactElement {
 	return (
-		<div className="flex flex-1 min-h-0 w-full">
-			<AppSidebar collapsible="icon" className="border-r top-12 h-[calc(100svh-3rem)]">
-				{sidebar}
-			</AppSidebar>
-			<AppSidebarInset className="flex flex-col flex-1 min-h-0 min-w-0">
-				<main className="flex-1 overflow-y-auto overflow-x-hidden bg-background">{children}</main>
-			</AppSidebarInset>
-		</div>
+		<AppSidebar collapsible="icon" className="border-r top-12 h-[calc(100svh-3rem)]">
+			{children}
+		</AppSidebar>
 	);
 });
