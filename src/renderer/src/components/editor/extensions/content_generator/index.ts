c5db767a6f124a5881608c15664118d1906f0b3c
harald bregu <harald.bregu@gmail.com>
2026-04-08 12:21:40 +0200

    feat: add content generator extension barrel exports

---
+++ src/renderer/src/components/editor/extensions/content_generator/index.ts
@@ -0,0 +1,4 @@
+export { ContentGeneratorNodeView } from './NodeView';
+export { ContentGeneratorExtension } from './input-extension';
+export type { ContentGeneratorOptions } from './input-extension';
+export type { ContentGeneratorAgentId } from './agents';
