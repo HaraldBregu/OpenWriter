ae5387aa5db40a9779562df60e352ea38e026a01
harald bregu <harald.bregu@gmail.com>
2026-04-09 00:40:06 +0200

    auto: add AppSidebarInsetLayout wrapper component

---
+++ src/renderer/src/components/app/sidebar/AppSidebarInsetLayout.tsx
@@ -0,0 +1,16 @@
+import { memo, type ReactNode, type ReactElement } from 'react';
+import { AppSidebarInset } from '../AppSidebar';
+
+interface AppSidebarInsetLayoutProps {
+	readonly children: ReactNode;
+}
+
+export const AppSidebarInsetLayout = memo(function AppSidebarInsetLayout({
+	children,
+}: AppSidebarInsetLayoutProps): ReactElement {
+	return (
+		<AppSidebarInset className="flex flex-col flex-1 min-h-0 min-w-0">
+			<main className="flex-1 overflow-y-auto overflow-x-hidden bg-background">{children}</main>
+		</AppSidebarInset>
+	);
+});
