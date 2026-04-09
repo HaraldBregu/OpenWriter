46492e840c8d1beafb0b2d410b362c8f5dcb9c0e
harald bregu <harald.bregu@gmail.com>
2026-04-09 23:34:36 +0200

    auto: remove unused ButtonGroup import from AppButtonGroup

--- src/renderer/src/components/app/AppButtonGroup.tsx
+++ src/renderer/src/components/app/AppButtonGroup.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
-import { ButtonGroup, ButtonGroupSeparator, ButtonGroupText } from '../ui/ButtonGroup';
+import { ButtonGroupSeparator, ButtonGroupText } from '../ui/ButtonGroup';
 import { cn } from '@/lib/utils';
 
 const buttonGroupVariants = cva(
