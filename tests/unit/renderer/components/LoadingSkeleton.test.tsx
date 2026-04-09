fd36f26b7909e4d16545834415ca5ee65dea9412
harald bregu <harald.bregu@gmail.com>
2026-04-09 14:48:17 +0200

    auto: update LoadingSkeleton test to use renamed AppLoadingSkeleton component

--- tests/unit/renderer/components/LoadingSkeleton.test.tsx
+++ tests/unit/renderer/components/LoadingSkeleton.test.tsx
@@ -10,7 +10,7 @@ import { AppLoadingSkeleton } from '../../../../src/renderer/src/components/AppL
 describe('LoadingSkeleton', () => {
 	it('should render the loading text', () => {
 		// Act
-		render(<LoadingSkeleton />);
+		render(<AppLoadingSkeleton />);
 
 		// Assert
 		expect(screen.getByText(/loading/i)).toBeInTheDocument();
@@ -18,7 +18,7 @@ describe('LoadingSkeleton', () => {
 
 	it('should render the spinner element', () => {
 		// Act
-		const { container } = render(<LoadingSkeleton />);
+		const { container } = render(<AppLoadingSkeleton />);
 
 		// Assert - look for the animated spinner div
 		const spinner = container.querySelector('.animate-spin');
