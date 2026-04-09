fd0ca3a8f0b9f108622f4619903f1b8474c17a50
harald bregu <harald.bregu@gmail.com>
2026-04-09 15:01:50 +0200

    auto: add developer settings page route

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -67,6 +67,7 @@ const WorkspacePage = lazy(() => import('./pages/settings/pages/WorkspacePage'))
 const SystemPage = lazy(() => import('./pages/settings/pages/SystemPage'));
 const ThemesPage = lazy(() => import('./pages/settings/pages/ThemesPage'));
 const EditorPage = lazy(() => import('./pages/settings/pages/EditorPage'));
+const DeveloperPage = lazy(() => import('./pages/settings/pages/DeveloperPage'));
 
 const FALLBACK_STARTUP_INFO: AppStartupInfo = {
 	startupCount: 0,
