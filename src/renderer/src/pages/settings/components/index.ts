ef5e06947c5d7d0196a3ed294653e7554c1c7c80
harald bregu <harald.bregu@gmail.com>
2026-04-09 11:12:58 +0200

    auto: add barrel export for settings components

---
+++ src/renderer/src/pages/settings/components/index.ts
@@ -0,0 +1,7 @@
+export { SectionHeader } from './SectionHeader';
+export { SettingRow } from './SettingRow';
+export { CollapsibleSection } from './CollapsibleSection';
+export { ThemeSegmentControl } from './ThemeSegmentControl';
+export { ProviderRow } from './ProviderRow';
+export { LanguageSelector } from './LanguageSelector';
+export { ThemeModeSelector } from './ThemeModeSelector';
