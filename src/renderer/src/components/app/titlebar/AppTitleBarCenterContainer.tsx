86b3f98153cf852494c6ac61368c1bae6839baa3
harald bregu <harald.bregu@gmail.com>
2026-04-09 14:26:04 +0200

    auto: add AppTitleBarCenterContainer component

---
+++ src/renderer/src/components/app/titlebar/AppTitleBarCenterContainer.tsx
@@ -0,0 +1,20 @@
+import { memo, type ReactNode, type ReactElement } from 'react';
+import { cn } from '@/lib/utils';
+
+interface AppTitleBarCenterContainerProps {
+	readonly className?: string;
+	readonly children: ReactNode;
+}
+
+export const AppTitleBarCenterContainer = memo(function AppTitleBarCenterContainer({
+	className,
+	children,
+}: AppTitleBarCenterContainerProps): ReactElement {
+	return (
+		<div
+			className={cn('absolute inset-0 flex items-center justify-center pointer-events-none', className)}
+		>
+			{children}
+		</div>
+	);
+});
