90c23431f13581119c97c75013658ddf16a6c4cb
harald bregu <harald.bregu@gmail.com>
2026-04-10 01:27:47 +0200

    auto: migrate AppCheckbox component from radix-ui to base-ui

--- src/renderer/src/components/app/AppCheckbox.tsx
+++ src/renderer/src/components/app/AppCheckbox.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import { Checkbox as CheckboxPrimitive } from '@base-ui/react/checkbox';
 import { Checkbox } from '../ui/Checkbox';
 import { cn } from '@/lib/utils';
 
 const AppCheckbox = React.memo(
 	React.forwardRef<
 		React.ElementRef<typeof CheckboxPrimitive.Root>,
-		React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+		CheckboxPrimitive.Root.Props
 	>(({ className, ...props }, ref) => <Checkbox ref={ref} className={cn(className)} {...props} />)
 );
 AppCheckbox.displayName = 'AppCheckbox';
