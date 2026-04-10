77469afac3b76a592bec32730781ef41ced137ec
harald bregu <harald.bregu@gmail.com>
2026-04-10 13:38:39 +0200

    auto: rename AppPageHeaderItems to PageHeaderItems in PageHeaderItems.tsx

--- src/renderer/src/components/app/base/PageHeaderItems.tsx
+++ src/renderer/src/components/app/base/PageHeaderItems.tsx
@@ -1,11 +1,11 @@
 import { memo, type ReactNode } from 'react';
 
-interface AppPageHeaderItemsProps {
+interface PageHeaderItemsProps {
 	readonly children: ReactNode;
 }
 
-export const AppPageHeaderItems = memo(function AppPageHeaderItems({
+export const PageHeaderItems = memo(function PageHeaderItems({
 	children,
-}: AppPageHeaderItemsProps): React.ReactElement {
+}: PageHeaderItemsProps): React.ReactElement {
 	return <div className="flex items-center gap-2">{children}</div>;
 });
