efc2a4c0a0796cca9ed26ae4063b1304cf4ec148
harald bregu <harald.bregu@gmail.com>
2026-04-12 00:28:12 +0200

    feat: export Gallery component from app index

--- src/renderer/src/components/app/index.ts
+++ src/renderer/src/components/app/index.ts
@@ -7,3 +7,6 @@ export { AppSidebarInsetLayout } from './sidebar/AppSidebarInsetLayout';
 export { AppIconOpenWriter } from './icons/AppIconOpenWriter';
 
 export { PageContainer, PageHeader, PageHeaderItems, PageHeaderTitle } from './base/Page';
+
+export { Gallery } from './Gallery';
+export type { GalleryImage, GallerySection } from './Gallery';
