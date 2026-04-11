89590575219704bcf48f3fabd6e98bf058ddb262
harald bregu <harald.bregu@gmail.com>
2026-04-11 23:51:46 +0200

    refactor: drop unused preview state from DataContent

--- src/renderer/src/pages/resources/Data/components/DataContent.tsx
+++ src/renderer/src/pages/resources/Data/components/DataContent.tsx
@@ -33,15 +33,7 @@ function EmptyState({
 export function DataContent(): ReactElement {
 	const { t } = useTranslation();
 	const section = RESOURCE_SECTIONS.data;
-	const {
-		resources,
-		isLoading,
-		error,
-		uploading,
-		handleUpload,
-		previewResource,
-		setPreviewResource,
-	} = useDataContext();
+	const { resources, isLoading, error, uploading, handleUpload } = useDataContext();
 
 	return (
 		<div className="flex flex-1 min-h-0 flex-col overflow-y-auto p-6">
