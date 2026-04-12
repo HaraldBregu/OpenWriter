c1b2910b728d3bf0ece2b247154b60ba8d1f1a21
harald bregu <harald.bregu@gmail.com>
2026-04-12 22:39:55 +0200

    refactor: add barrel export for Files context module

---
+++ src/renderer/src/pages/resources/Files/context/index.ts
@@ -0,0 +1,10 @@
+export { FilesProvider } from './Provider';
+export { useFilesContext } from './FilesContext';
+export type { FilesContextValue } from './types';
+export type {
+	FileEntry,
+	FileTypeFilter,
+	SortDirection,
+	SortKey,
+	ViewMode,
+} from './types';
