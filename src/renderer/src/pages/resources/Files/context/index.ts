d52ad8ceee36d8ec24bb21fe0164bdb365c317f3
harald bregu <harald.bregu@gmail.com>
2026-04-12 23:11:13 +0200

    refactor: update context index to export renamed useContext hook

--- src/renderer/src/pages/resources/Files/context/index.ts
+++ src/renderer/src/pages/resources/Files/context/index.ts
@@ -1,4 +1,4 @@
-export { useFilesContext } from '../hooks/use-context';
+export { useContext } from '../hooks/use-context';
 export { filesReducer } from './reducer';
 export { initialFilesState } from './state';
 export type { FilesContextValue } from './types';
