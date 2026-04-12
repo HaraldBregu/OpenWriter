83862811aec3a4d04b196c94833ebf6c397e52c3
harald bregu <harald.bregu@gmail.com>
2026-04-12 22:45:18 +0200

    refactor: remove obsolete Files provider and hook modules

--- src/renderer/src/pages/resources/Files/context/index.ts
+++ src/renderer/src/pages/resources/Files/context/index.ts
@@ -1,10 +1,7 @@
-export { FilesProvider } from './Provider';
 export { useFilesContext } from './FilesContext';
+export { filesReducer } from './reducer';
+export { initialFilesState } from './state';
 export type { FilesContextValue } from './types';
-export type {
-	FileEntry,
-	FileTypeFilter,
-	SortDirection,
-	SortKey,
-	ViewMode,
-} from './types';
+export type { FileEntry, FileTypeFilter, SortDirection, SortKey, ViewMode } from './types';
+export type { FilesReducerState } from './state';
+export type { FilesAction } from './actions';
