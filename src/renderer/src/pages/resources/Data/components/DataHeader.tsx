6026da480f763a8926a211d0e4c7d50529b4026e
harald bregu <harald.bregu@gmail.com>
2026-04-13 00:22:14 +0200

    fix: update DataHeader to use renamed useContext hook

--- src/renderer/src/pages/resources/Data/components/DataHeader.tsx
+++ src/renderer/src/pages/resources/Data/components/DataHeader.tsx
@@ -19,7 +19,7 @@ export function DataHeader(): ReactElement {
 		handleUpload,
 		handleToggleEdit,
 		handleIndex,
-	} = useDataContext();
+	} = useContext();
 
 	return (
 		<div className="flex shrink-0 items-center justify-between border-b px-6 py-4">
