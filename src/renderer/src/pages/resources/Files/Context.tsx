da966880314e10af3977045f01f5413ff2077e61
harald bregu <harald.bregu@gmail.com>
2026-04-12 23:14:10 +0200

    refactor: update FilesContextValue import path to context/types

--- src/renderer/src/pages/resources/Files/Context.tsx
+++ src/renderer/src/pages/resources/Files/Context.tsx
@@ -1,4 +1,4 @@
 import { createContext } from 'react';
-import type { FilesContextValue } from './types';
+import type { FilesContextValue } from './context/types';
 
 export const Context = createContext<FilesContextValue | null>(null);
