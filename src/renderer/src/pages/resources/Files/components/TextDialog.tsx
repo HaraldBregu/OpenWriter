749f4c8c62212cfd70dc0ab9cd98bcd9c041072c
harald bregu <harald.bregu@gmail.com>
2026-04-12 23:22:52 +0200

    refactor: update useDelete import path in TextDialog

--- src/renderer/src/pages/resources/Files/components/TextDialog.tsx
+++ src/renderer/src/pages/resources/Files/components/TextDialog.tsx
@@ -18,7 +18,7 @@ import {
 } from '../../shared/resource-preview-utils';
 import { formatBytes, formatDate } from '../../shared/resource-utils';
 import { useContext } from '../hooks/use-context';
-import { useDelete } from '../hooks/use-file-delete';
+import { useDelete } from '../hooks/use-delete';
 import { DetailRow } from './DetailRow';
 import { PreviewLoading, PreviewError } from './PreviewStates';
 
