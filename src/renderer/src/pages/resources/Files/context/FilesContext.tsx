787b783e63bbbaeddba688df8d10ef96f901ca22
harald bregu <harald.bregu@gmail.com>
2026-04-12 22:47:38 +0200

    refactor: remove useFilesContext hook from FilesContext module

--- src/renderer/src/pages/resources/Files/context/FilesContext.tsx
+++ src/renderer/src/pages/resources/Files/context/FilesContext.tsx
@@ -1,12 +1,4 @@
-import { createContext, useContext } from 'react';
+import { createContext } from 'react';
 import type { FilesContextValue } from './types';
 
 export const FilesContext = createContext<FilesContextValue | null>(null);
-
-export function useFilesContext(): FilesContextValue {
-	const context = useContext(FilesContext);
-	if (!context) {
-		throw new Error('useFilesContext must be used within a FilesProvider');
-	}
-	return context;
-}
