1c456380e99b3b45ed04e95c71715de03c6da973
harald bregu <harald.bregu@gmail.com>
2026-04-13 00:21:34 +0200

    feat: add knowledge base builder for markdown vectorization

---
+++ src/main/data/index.ts
@@ -0,0 +1,7 @@
+export {
+	buildKnowledgeBase,
+	type KnowledgeBaseBuildPhase,
+	type KnowledgeBaseBuildProgress,
+	type KnowledgeBaseBuildInput,
+	type KnowledgeBaseBuildResult,
+} from './knowledge-base-builder';
