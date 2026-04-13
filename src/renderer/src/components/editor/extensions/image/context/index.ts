cf60def491c69ad859af8378edb7d1a9ba784640
harald bregu <harald.bregu@gmail.com>
2026-04-14 00:39:35 +0200

    feat: add barrel exports for image extension context module

---
+++ src/renderer/src/components/editor/extensions/image/context/index.ts
@@ -0,0 +1,5 @@
+export type { ImageState } from './state';
+export type { ImageAction } from './actions';
+export { imageReducer } from './reducer';
+export { ImageContext } from './context';
+export type { ImageContextValue } from './context';
