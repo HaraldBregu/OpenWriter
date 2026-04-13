d93c04a2263aa9aca4334899505c86dac53838f3
harald bregu <harald.bregu@gmail.com>
2026-04-13 09:29:05 +0200

    refactor: rename SidebarContainer to Sidebar in sidebar component

--- src/renderer/src/components/app/sidebar/Sidebar.tsx
+++ src/renderer/src/components/app/sidebar/Sidebar.tsx
@@ -1,11 +1,11 @@
 import { memo, type ReactNode, type ReactElement } from 'react';
 
-interface SidebarContainerProps {
+interface SidebarProps {
 	readonly children: ReactNode;
 }
 
-export const SidebarContainer = memo(function SidebarContainer({
+export const Sidebar = memo(function Sidebar({
 	children,
-}: SidebarContainerProps): ReactElement {
+}: SidebarProps): ReactElement {
 	return <div className="flex flex-1 min-h-0 w-full">{children}</div>;
 });
