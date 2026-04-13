c830668c28302432493ad6577d41ad1e958a110e
harald bregu <harald.bregu@gmail.com>
2026-04-13 22:23:56 +0200

    refactor: remove unused AgentDropdown export from content generator components

--- src/renderer/src/components/editor/extensions/content_generator/components/index.ts
+++ src/renderer/src/components/editor/extensions/content_generator/components/index.ts
@@ -1,4 +1,3 @@
-export { AgentDropdown } from './AgentDropdown';
 export { ImageAttachmentBar } from './ImageAttachmentBar';
 export { PromptFooter } from './PromptFooter';
 export { PromptHeader } from './PromptHeader';
