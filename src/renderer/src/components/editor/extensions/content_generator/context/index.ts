da14b826fa17c3d54cddb51070c33a02384be22f
harald bregu <harald.bregu@gmail.com>
2026-04-13 21:26:37 +0200

    refactor: add barrel export for content generator context module

---
+++ src/renderer/src/components/editor/extensions/content_generator/context/index.ts
@@ -0,0 +1,5 @@
+export type { ContentGeneratorState } from './state';
+export type { ContentGeneratorAction } from './actions';
+export { contentGeneratorReducer } from './reducer';
+export { ContentGeneratorContext } from './content-generator-context';
+export type { ContentGeneratorContextValue } from './content-generator-context';
