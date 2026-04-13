3589b08e077c087bb622100bda9adb45050d33b3
harald bregu <harald.bregu@gmail.com>
2026-04-13 11:38:07 +0200

    refactor: simplify useDocumentState to use unified document context

--- src/renderer/src/pages/document/hooks/use-document-state.ts
+++ src/renderer/src/pages/document/hooks/use-document-state.ts
@@ -1,11 +1,6 @@
-import { useContext } from 'react';
-import { DocumentStateContext } from '../providers';
+import { useDocumentContext } from '../providers';
 import type { DocumentState } from '../context/state';
 
 export function useDocumentState(): DocumentState {
-	const ctx = useContext(DocumentStateContext);
-	if (ctx === null) {
-		throw new Error('useDocumentState must be used within a DocumentProvider');
-	}
-	return ctx;
+	return useDocumentContext().state;
 }
