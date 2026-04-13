b9dea76406beb6a184fe727ebf51070068df471c
harald bregu <harald.bregu@gmail.com>
2026-04-13 11:31:58 +0200

    refactor: consolidate document provider exports into single module

--- src/renderer/src/pages/document/providers/index.ts
+++ src/renderer/src/pages/document/providers/index.ts
@@ -1,4 +1,8 @@
-export { DocumentProvider, DocumentStateContext, DocumentDispatchContext } from './Document';
-export { EditorInstanceProvider, useEditorInstance } from './Editor';
-export { SidebarVisibilityProvider, useSidebarVisibility } from './Sidebar';
-export type { ActiveSidebar } from './Sidebar';
+export {
+	DocumentProvider,
+	DocumentStateContext,
+	DocumentDispatchContext,
+	useEditorInstance,
+	useSidebarVisibility,
+} from './Document';
+export type { ActiveSidebar } from './Document';
