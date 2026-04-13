9c3db396017096be463a91c8966d958a2ed38278
harald bregu <harald.bregu@gmail.com>
2026-04-13 11:45:08 +0200

    refactor: use renamed useContext hook in useDocumentDispatch

--- src/renderer/src/pages/document/hooks/use-document-dispatch.ts
+++ src/renderer/src/pages/document/hooks/use-document-dispatch.ts
@@ -1,7 +1,7 @@
 import type { Dispatch } from 'react';
-import { useDocumentContext } from '../providers';
+import { useContext } from '../providers';
 import type { DocumentAction } from '../context/actions';
 
 export function useDocumentDispatch(): Dispatch<DocumentAction> {
-	return useDocumentContext().dispatch;
+	return useContext().dispatch;
 }
