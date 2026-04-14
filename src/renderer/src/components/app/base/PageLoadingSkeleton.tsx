6242bf3ba9b22625a233dcc86d838019d2f0b56e
harald bregu <harald.bregu@gmail.com>
2026-04-14 14:40:27 +0200

    feat: replace LoadingSkeleton with PageLoadingSkeleton

---
+++ src/renderer/src/components/app/base/PageLoadingSkeleton.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Skeleton } from '@/components/ui/Skeleton';
+import { PageContainer, PageHeader, PageHeaderTitle, PageHeaderItems, PageBody } from './Page';
+
+export const PageLoadingSkeleton = React.memo(function PageLoadingSkeleton() {
+	return (
+		<PageContainer>
+			<PageHeader>
+				<PageHeaderTitle>
+					<Skeleton className="h-6 w-48" />
+				</PageHeaderTitle>
+				<PageHeaderItems>
+					<Skeleton className="h-8 w-20 rounded-md" />
+					<Skeleton className="h-8 w-8 rounded-md" />
+				</PageHeaderItems>
+			</PageHeader>
+			<PageBody className="p-6">
+				<div className="flex flex-col gap-4 max-w-3xl">
+					<Skeleton className="h-8 w-1/2" />
+					<Skeleton className="h-4 w-2/3" />
+					<div className="space-y-2 pt-4">
+						<Skeleton className="h-3 w-full" />
+						<Skeleton className="h-3 w-full" />
+						<Skeleton className="h-3 w-11/12" />
+						<Skeleton className="h-3 w-10/12" />
+						<Skeleton className="h-3 w-full" />
+						<Skeleton className="h-3 w-9/12" />
+					</div>
+				</div>
+			</PageBody>
+		</PageContainer>
+	);
+});
+PageLoadingSkeleton.displayName = 'PageLoadingSkeleton';
