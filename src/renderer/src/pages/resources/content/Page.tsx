07d53f642303eb87c29e17212d471a1114a60508
harald bregu <harald.bregu@gmail.com>
2026-04-14 23:04:25 +0200

    feat: add markdown dialog open state in resources content page

--- src/renderer/src/pages/resources/content/Page.tsx
+++ src/renderer/src/pages/resources/content/Page.tsx
@@ -67,6 +67,7 @@ function formatShortDate(timestamp: number): string {
 function PageContent(): ReactElement {
 	const { t } = useTranslation();
 	const section = RESOURCE_SECTIONS.content;
+	const [markdownDialogOpen, setMarkdownDialogOpen] = useState(false);
 	const [fileDialogOpen, setFileDialogOpen] = useState(false);
 	const {
 		folders,
