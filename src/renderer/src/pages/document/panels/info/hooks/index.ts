f2dbdfe32296b03c1125278d5497afb38857c95f
harald bregu <harald.bregu@gmail.com>
2026-04-15 22:23:52 +0200

    refactor: add info panel hooks barrel export

---
+++ src/renderer/src/pages/document/panels/info/hooks/index.ts
@@ -0,0 +1,5 @@
+export { useInfoState } from './use-info-state';
+export { useInfoDispatch } from './use-info-dispatch';
+export { useDocumentConfig } from './use-document-config';
+export { useImageUpload } from './use-image-upload';
+export { useDeleteDocument } from './use-delete-document';
