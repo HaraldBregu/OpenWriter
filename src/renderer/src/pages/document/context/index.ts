5bd7a0e99e40f9e1d946230cf2fd251989e79aa5
harald bregu <harald.bregu@gmail.com>
2026-04-15 20:29:09 +0200

    feat: export DocumentContext and related types from context barrel

--- src/renderer/src/pages/document/context/index.ts
+++ src/renderer/src/pages/document/context/index.ts
@@ -2,3 +2,4 @@ export type { DocumentState } from './state';
 export { INITIAL_DOCUMENT_STATE } from './state';
 export type { DocumentAction } from './actions';
 export { documentReducer } from './reducer';
+export { DocumentContext, type ActiveSidebar, type ContextValue } from './context';
