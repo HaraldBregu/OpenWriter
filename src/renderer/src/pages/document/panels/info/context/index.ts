070c25d9f2c63eb1d1f6074232ced7d4b4ec115c
harald bregu <harald.bregu@gmail.com>
2026-04-15 22:21:44 +0200

    refactor: add info panel context barrel exports

---
+++ src/renderer/src/pages/document/panels/info/context/index.ts
@@ -0,0 +1,5 @@
+export { InfoStateContext, InfoDispatchContext } from './contexts';
+export { infoReducer } from './reducer';
+export { INITIAL_INFO_STATE } from './state';
+export type { InfoState, InfoPreviewImage } from './state';
+export type { InfoAction } from './actions';
