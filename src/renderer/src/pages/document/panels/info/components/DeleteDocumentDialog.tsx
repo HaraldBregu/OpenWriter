fddea0de507c35b05d09a7d9a1f08aef00cfae40
harald bregu <harald.bregu@gmail.com>
2026-04-15 22:25:28 +0200

    refactor: add info panel delete document dialog component

---
+++ src/renderer/src/pages/document/panels/info/components/DeleteDocumentDialog.tsx
@@ -0,0 +1,47 @@
+import { useTranslation } from 'react-i18next';
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+} from '@/components/ui/AlertDialog';
+import { useDeleteDocument } from '../hooks/use-delete-document';
+import { useInfoDispatch } from '../hooks/use-info-dispatch';
+import { useInfoState } from '../hooks/use-info-state';
+
+export function DeleteDocumentDialog(): React.ReactElement {
+	const { t } = useTranslation();
+	const { confirmDeleteOpen, isDeleting } = useInfoState();
+	const dispatch = useInfoDispatch();
+	const handleDeletePermanently = useDeleteDocument();
+
+	return (
+		<AlertDialog
+			open={confirmDeleteOpen}
+			onOpenChange={(open) => dispatch({ type: 'CONFIRM_DELETE_OPEN_CHANGED', open })}
+		>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>{t('configSidebar.deleteDocumentTitle')}</AlertDialogTitle>
+					<AlertDialogDescription>
+						{t('configSidebar.deleteDocumentConfirm')}
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel disabled={isDeleting}>{t('common.cancel')}</AlertDialogCancel>
+					<AlertDialogAction
+						className="bg-destructive text-destructive-foreground hover:bg-destructive/90"
+						onClick={handleDeletePermanently}
+						disabled={isDeleting}
+					>
+						{t('configSidebar.deletePermanently')}
+					</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
+}
