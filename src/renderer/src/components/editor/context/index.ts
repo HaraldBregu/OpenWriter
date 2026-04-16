bfd894a319749dcab5a7610cded0e447b01e86ba
harald bregu <harald.bregu@gmail.com>
2026-04-16 22:58:47 +0200

    feat: add editor context barrel exports

---
+++ src/renderer/src/components/editor/context/index.ts
@@ -0,0 +1,5 @@
+export type { EditorState, HoveredBlock } from './state';
+export type { EditorAction } from './actions';
+export { editorReducer } from './reducer';
+export { EditorContext } from './context';
+export type { EditorContextValue } from './context';
