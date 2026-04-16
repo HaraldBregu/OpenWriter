4ad40ecbf77046ec54eb4a24b722a8dc63f243df
harald bregu <harald.bregu@gmail.com>
2026-04-16 23:20:03 +0200

    refactor: update import paths in image index

--- src/renderer/src/components/editor/components/image/index.ts
+++ src/renderer/src/components/editor/components/image/index.ts
@@ -1,6 +1,6 @@
 export { ImageNodeView } from './NodeView';
-export { ImageExtension } from './input-extension';
-export type { ImageExtensionOptions } from './input-extension';
-export { createImageDropPastePlugin, fileToDataUri } from './image-drop-paste-plugin';
-export type { ImageInsertHandler } from './image-drop-paste-plugin';
+export { ImageExtension } from '../../extensions/image-extension';
+export type { ImageExtensionOptions } from '../../extensions/image-extension';
+export { createImageDropPastePlugin, fileToDataUri } from '../../plugins/image-drop-paste-plugin';
+export type { ImageInsertHandler } from '../../plugins/image-drop-paste-plugin';
 export { ImageEditor } from './components/ImageEditor';
