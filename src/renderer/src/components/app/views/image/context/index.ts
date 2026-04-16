bc27bfc14d784017df991bd6a5c58b5aed3d8901
harald bregu <harald.bregu@gmail.com>
2026-04-17 00:38:30 +0200

    feat: add barrel export for image editor context

---
+++ src/renderer/src/components/app/views/image/context/index.ts
@@ -0,0 +1,5 @@
+export type { EditMode, ImageEditorState } from './state';
+export type { ImageEditorAction } from './actions';
+export { imageEditorReducer } from './reducer';
+export { ImageEditorContext } from './context';
+export type { ImageEditorContextValue, ImageEditorRefs } from './context';
