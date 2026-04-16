5eef46ba331bfcd93825b8a7136a1e6f66aae5d7
harald bregu <harald.bregu@gmail.com>
2026-04-16 23:20:57 +0200

    refactor: update import paths in content_generator index

--- src/renderer/src/components/editor/components/content_generator/index.ts
+++ src/renderer/src/components/editor/components/content_generator/index.ts
@@ -1,4 +1,4 @@
 export { NodeView } from './NodeView';
-export { ContentGeneratorExtension } from './input-extension';
-export type { ContentGeneratorOptions, ContentGeneratorStorage } from './input-extension';
+export { ContentGeneratorExtension } from '../../extensions/content-generator-extension';
+export type { ContentGeneratorOptions, ContentGeneratorStorage } from '../../extensions/content-generator-extension';
 export type { ContentGeneratorAgentId } from './agents';
