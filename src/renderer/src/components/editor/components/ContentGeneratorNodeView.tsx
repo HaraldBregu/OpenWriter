844e2c736fd3e1d336603da6f25b3c8f3c369da2
harald bregu <harald.bregu@gmail.com>
2026-04-17 00:28:32 +0200

    refactor: update ContentGeneratorNodeView imports to flat paths

--- src/renderer/src/components/editor/components/ContentGeneratorNodeView.tsx
+++ src/renderer/src/components/editor/components/ContentGeneratorNodeView.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { NodeViewWrapper } from '@tiptap/react';
 import type { NodeViewProps } from '@tiptap/react';
-import { Provider } from './content_generator/Provider';
-import { CardNodeView } from './content_generator/CardNodeView';
+import { ContentGeneratorProvider } from './ContentGeneratorProvider';
+import { CardNodeView } from './CardNodeView';
 
 export function ContentGeneratorNodeView(props: NodeViewProps): React.JSX.Element {
 	return (
