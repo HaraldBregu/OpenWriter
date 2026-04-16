707f4cfe7ac9700a560bd089c2cc2cb77642d778
harald bregu <harald.bregu@gmail.com>
2026-04-16 23:45:44 +0200

    refactor: export image context modules from editor context

--- src/renderer/src/components/editor/context/index.ts
+++ src/renderer/src/components/editor/context/index.ts
@@ -3,3 +3,10 @@ export type { EditorAction, ContentGeneratorAction } from './actions';
 export { editorReducer, contentGeneratorReducer } from './reducer';
 export { EditorContext, ContentGeneratorContext } from './context';
 export type { EditorContextValue, ContentGeneratorContextValue } from './context';
+
+export type { ImageState } from './image-state';
+export type { ImageAction } from './image-actions';
+export { imageReducer } from './image-reducer';
+export { ImageContext } from './image-context';
+export type { ImageContextValue } from './image-context';
+export { ImageProvider } from './ImageProvider';
