d47b03b0338c7246d9784e74f2217cc391edec51
harald bregu <harald.bregu@gmail.com>
2026-04-16 23:46:08 +0200

    refactor: update Provider import to ImageProvider from context

--- src/renderer/src/components/editor/components/image/NodeView.tsx
+++ src/renderer/src/components/editor/components/image/NodeView.tsx
@@ -8,7 +8,7 @@ import { Button } from '@/components/ui/Button';
 import { Card, CardContent } from '@/components/ui/Card';
 import { Empty, EmptyHeader, EmptyMedia, EmptyTitle } from '@/components/ui/Empty';
 import { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider } from '@/components/ui/Tooltip';
-import { Provider } from './Provider';
+import { ImageProvider } from '../../context/ImageProvider';
 import { useImage } from '../../hooks/use-image';
 import { ImageEditor } from './components/ImageEditor';
 import { ImagePreviewDialog } from './components';
