9d144700dffd7ab246329096221bc8ba4da972fd
harald bregu <harald.bregu@gmail.com>
2026-04-17 00:45:04 +0200

    refactor: update ContentGenerator import path in hook

--- src/renderer/src/components/editor/hooks/use-content-generator.ts
+++ src/renderer/src/components/editor/hooks/use-content-generator.ts
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
-import { ContentGeneratorContext } from '../context/context';
-import type { ContentGeneratorContextValue } from '../context/context';
+import { ContentGeneratorContext } from '../../app/views/prompt/context/context';
+import type { ContentGeneratorContextValue } from '../../app/views/prompt/context/context';
 
 export function useContentGenerator(): ContentGeneratorContextValue {
 	const context = useContext(ContentGeneratorContext);
