372f69d37871c7be3931363ad4921a65caa2867e
harald bregu <harald.bregu@gmail.com>
2026-04-17 00:26:06 +0200

    refactor: update NodeView export in content_generator index

--- src/renderer/src/components/editor/components/content_generator/index.ts
+++ src/renderer/src/components/editor/components/content_generator/index.ts
@@ -1,4 +1,4 @@
-export { NodeView } from './NodeView';
+export { ContentGeneratorNodeView } from '../ContentGeneratorNodeView';
 export { ContentGeneratorExtension } from '../../extensions/content-generator-extension';
 export type { ContentGeneratorOptions, ContentGeneratorStorage } from '../../extensions/content-generator-extension';
 export type { ContentGeneratorAgentId } from './agents';
