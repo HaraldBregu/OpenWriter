e5e7a1991249d06891e560ca6cf97e6bb7c094a7
harald bregu <harald.bregu@gmail.com>
2026-04-16 23:51:57 +0200

    refactor: update ImageEditor import path in image index

--- src/renderer/src/components/editor/components/image/index.ts
+++ src/renderer/src/components/editor/components/image/index.ts
@@ -3,4 +3,4 @@ export { ImageExtension } from '../../extensions/image-extension';
 export type { ImageExtensionOptions } from '../../extensions/image-extension';
 export { createImageDropPastePlugin, fileToDataUri } from '../../plugins/image-drop-paste-plugin';
 export type { ImageInsertHandler } from '../../plugins/image-drop-paste-plugin';
-export { ImageEditor } from './components/ImageEditor';
+export { ImageEditor } from '../ImageEditor';
