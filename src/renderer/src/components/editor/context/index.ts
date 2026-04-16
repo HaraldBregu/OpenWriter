e5334a5e7fb3b9449e2474700a110483cff13c7f
harald bregu <harald.bregu@gmail.com>
2026-04-17 00:57:14 +0200

    refactor: remove unused image editor module

--- src/renderer/src/components/editor/context/index.ts
+++ src/renderer/src/components/editor/context/index.ts
@@ -3,10 +3,3 @@ export type { EditorAction } from './actions';
 export { editorReducer } from './reducer';
 export { EditorContext } from './context';
 export type { EditorContextValue } from './context';
-
-export type { ImageState } from './image-state';
-export type { ImageAction } from './image-actions';
-export { imageReducer } from './image-reducer';
-export { ImageContext } from './image-context';
-export type { ImageContextValue } from './image-context';
-export { ImageProvider } from './ImageProvider';
