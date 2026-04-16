5213cd687496eb7e5d2015bb8f0917e5d47f1aa6
harald bregu <harald.bregu@gmail.com>
2026-04-16 09:54:37 +0200

    refactor: expand context barrel exports with Context, DataProvider, and new types

--- src/renderer/src/pages/resources/data/context/index.ts
+++ src/renderer/src/pages/resources/data/context/index.ts
@@ -1,2 +1,4 @@
+export { Context } from './Context';
+export { DataProvider } from './Provider';
 export { useContext } from '../hooks/use-context';
-export type { DataContextValue, ResourceInfo, SortDirection, SortKey } from './types';
+export type { DataContextValue, IndexingInfo, KnowledgeBase, ResourceInfo, SortDirection, SortKey } from './types';
