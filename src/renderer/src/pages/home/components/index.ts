2096d63555579461838a05c37e369b5a53ba222d
harald bregu <harald.bregu@gmail.com>
2026-04-16 11:55:11 +0200

    feat: add barrel export for home page components

---
+++ src/renderer/src/pages/home/components/index.ts
@@ -0,0 +1,4 @@
+export { CategoryCard } from './CategoryCard';
+export { QuickActions } from './QuickActions';
+export { RecentDocuments } from './RecentDocuments';
+export { TipSection } from './TipSection';
