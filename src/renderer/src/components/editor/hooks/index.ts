409594565b0cf054441c0c617c30922810dba35a
harald bregu <harald.bregu@gmail.com>
2026-04-16 23:23:04 +0200

    refactor: export additional hooks from editor hooks index

--- src/renderer/src/components/editor/hooks/index.ts
+++ src/renderer/src/components/editor/hooks/index.ts
@@ -1,2 +1,8 @@
 export { useEditorContext } from './use-editor-context';
 export { useBlockHover } from './use-block-hover';
+export { useImage } from './use-image';
+export { useImageActions } from './use-image-actions';
+export { useImageCanvas } from './use-image-canvas';
+export { useContentGenerator } from './use-content-generator';
+export { useContentGeneratorActions } from './use-content-generator-actions';
+export { useTextareaSetup } from './use-textarea-setup';
