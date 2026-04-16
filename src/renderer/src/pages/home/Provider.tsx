15742e68b7dac619903171cf1a999899e8b2f460
harald bregu <harald.bregu@gmail.com>
2026-04-16 12:21:37 +0200

    feat: add Provider component for home page context

---
+++ src/renderer/src/pages/home/Provider.tsx
@@ -0,0 +1,19 @@
+import React, { useReducer, useMemo, type ReactNode } from 'react';
+import { homeReducer } from './context/reducer';
+import { INITIAL_HOME_STATE } from './context/state';
+import { HomeContext, type ContextValue } from './context/context';
+
+interface ProviderProps {
+	readonly children: ReactNode;
+}
+
+export function Provider({ children }: ProviderProps): React.JSX.Element {
+	const [state, dispatch] = useReducer(homeReducer, INITIAL_HOME_STATE);
+	const value = useMemo<ContextValue>(() => ({ state, dispatch }), [state, dispatch]);
+
+	return (
+		<HomeContext.Provider value={value}>
+			{children}
+		</HomeContext.Provider>
+	);
+}
