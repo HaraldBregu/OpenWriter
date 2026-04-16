d26044d5e81edb7fb065f91c58fee108b67cc337
harald bregu <harald.bregu@gmail.com>
2026-04-16 23:31:18 +0200

    refactor: export content generator context from editor context index

--- src/renderer/src/components/editor/context/index.ts
+++ src/renderer/src/components/editor/context/index.ts
@@ -3,3 +3,9 @@ export type { EditorAction } from './actions';
 export { editorReducer } from './reducer';
 export { EditorContext } from './context';
 export type { EditorContextValue } from './context';
+
+export type { ContentGeneratorState } from './content-generator-state';
+export type { ContentGeneratorAction } from './content-generator-actions';
+export { contentGeneratorReducer } from './content-generator-reducer';
+export { ContentGeneratorContext } from './content-generator-context';
+export type { ContentGeneratorContextValue } from './content-generator-context';
