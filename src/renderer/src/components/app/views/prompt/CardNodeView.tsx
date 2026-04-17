af229bee19d4eae87c37138cd9430c7bfa12a237
harald bregu <harald.bregu@gmail.com>
2026-04-17 08:57:50 +0200

    refactor: remove unused CropOverlay and wire CardNodeView provider

--- src/renderer/src/components/app/views/prompt/CardNodeView.tsx
+++ src/renderer/src/components/app/views/prompt/CardNodeView.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import type { NodeViewProps } from '@tiptap/react';
 import { cn } from '@/lib/utils';
 import { Card, CardContent, CardFooter } from '@/components/ui/Card';
 import { Textarea } from '@/components/ui/Textarea';
 import { FileUpload, FileUploadDropzone, FileUploadTrigger } from '@/components/ui/FileUpload';
 import { useContentGenerator } from '../../../editor/hooks/use-content-generator';
+import { ContentGeneratorProvider } from './Provider';
 import { CardNodeViewHeader } from './CardNodeViewHeader';
 import {
 	DropdownMenu,
