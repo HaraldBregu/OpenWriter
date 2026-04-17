9d2a207512048f42da8081addfb229cf5894e6a8
harald bregu <harald.bregu@gmail.com>
2026-04-17 13:23:34 +0200

    refactor: fix image view import path in ImageNodeView

--- src/renderer/src/components/app/editor/nodes/ImageNodeView.tsx
+++ src/renderer/src/components/app/editor/nodes/ImageNodeView.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NodeViewWrapper } from '@tiptap/react';
 import type { NodeViewProps } from '@tiptap/react';
-import { ImageView } from '../../app/views/image';
+import { ImageView } from '../../views/image';
 
 export function ImageNodeView(props: NodeViewProps): React.JSX.Element {
 	return (
