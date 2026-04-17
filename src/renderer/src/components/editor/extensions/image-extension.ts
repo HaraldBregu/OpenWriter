4970d7d29c6e9d7864f584b2768ce8a5ba025b4c
harald bregu <harald.bregu@gmail.com>
2026-04-17 09:04:22 +0200

    refactor: move ImageNodeView and PlaceholderNodeView to nodes dir

--- src/renderer/src/components/editor/extensions/image-extension.ts
+++ src/renderer/src/components/editor/extensions/image-extension.ts
@@ -1,6 +1,6 @@
 import { Node, mergeAttributes } from '@tiptap/core';
 import { ReactNodeViewRenderer } from '@tiptap/react';
-import { ImageNodeView } from '../components/ImageNodeView';
+import { ImageNodeView } from '../nodes/ImageNodeView';
 import {
 	createImageDropPastePlugin,
 	fileToDataUri,
