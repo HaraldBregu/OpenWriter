e1516952bb8d5a179b22c0b6721544092c3fe2bf
harald bregu <harald.bregu@gmail.com>
2026-04-17 12:26:33 +0200

    refactor: remove EditorContainer export from app index

--- src/renderer/src/components/app/index.ts
+++ src/renderer/src/components/app/index.ts
@@ -1,5 +1,3 @@
-export { EditorContainer } from './base/Editor';
-
 export { AppIconOpenWriter } from './icons/AppIconOpenWriter';
 
 export { PageContainer, PageHeader, PageHeaderItems, PageHeaderTitle } from './base/Page';
