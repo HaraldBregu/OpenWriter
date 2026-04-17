c7c3ea39e596637e838953418137b6b0f80a6c3f
harald bregu <harald.bregu@gmail.com>
2026-04-17 08:33:24 +0200

    feat: export InsertImageDialog and ImagePreviewDialog from dialogs index

--- src/renderer/src/components/app/dialogs/index.ts
+++ src/renderer/src/components/app/dialogs/index.ts
@@ -1,2 +1,4 @@
 export { DeleteConfirmDialog } from './DeleteConfirmDialog';
 export type { DeleteConfirmDialogProps } from './DeleteConfirmDialog';
+export { InsertImageDialog } from './InsertImageDialog';
+export { ImagePreviewDialog } from './ImagePreviewDialog';
