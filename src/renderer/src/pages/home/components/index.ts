973b443e83595e5f75068db0b869266cb4401f71
harald bregu <harald.bregu@gmail.com>
2026-04-17 11:53:24 +0200

    feat: export new home page sections

--- src/renderer/src/pages/home/components/index.ts
+++ src/renderer/src/pages/home/components/index.ts
@@ -2,3 +2,6 @@ export { CategoryCard } from './CategoryCard';
 export { QuickActions } from './QuickActions';
 export { RecentDocuments } from './RecentDocuments';
 export { TipSection } from './TipSection';
+export { ResourcesSection } from './ResourcesSection';
+export { ToolsSection } from './ToolsSection';
+export { DebugSection } from './DebugSection';
