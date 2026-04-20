3cbfde07faa4c5fe563948bc3a83d547d25d0c77
harald bregu <harald.bregu@gmail.com>
2026-04-20 11:39:00 +0200

    chore: add command_palette barrel exports for provider and hook

---
+++ src/renderer/src/components/app/command_palette/index.ts
@@ -0,0 +1,3 @@
+export { CommandPaletteProvider } from './CommandPaletteProvider';
+export { useCommandPalette } from './CommandPaletteContext';
+export type { CommandPaletteId } from './CommandPaletteContext';
