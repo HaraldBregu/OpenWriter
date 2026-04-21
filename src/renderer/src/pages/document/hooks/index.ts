3fe9e19d34a4967028b6c99358c80002cdd41b52
harald bregu <harald.bregu@gmail.com>
2026-04-21 11:59:18 +0200

    auto: drop unused useDocumentPersistence hook

--- src/renderer/src/pages/document/hooks/index.ts
+++ src/renderer/src/pages/document/hooks/index.ts
@@ -6,7 +6,6 @@ export { useDocumentState } from './use-document-state';
 export { useDocumentDispatch } from './use-document-dispatch';
 export { useDocumentUI } from './use-document-ui';
 export { useDocumentActions } from './use-document-actions';
-export { useDocumentPersistence } from './use-document-persistence';
 export { useDocumentHistory, type HistoryEntry } from './use-document-history';
 export { useEditor } from './use-editor';
 export type { EditorActions } from './use-editor';
