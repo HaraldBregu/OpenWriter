16987dfcfa4885ea5313e9dd2150d474330296ab
harald bregu <harald.bregu@gmail.com>
2026-04-21 15:32:47 +0200

    auto: add Page barrel exports for components and context

---
+++ src/renderer/src/components/app/base/page/index.ts
@@ -0,0 +1,11 @@
+export {
+	PageContainer,
+	PageHeader,
+	PageHeaderTitle,
+	PageHeaderItems,
+	PageSubHeader,
+	PageBody,
+	PageSidebar,
+	PageSidebarInset,
+} from './Page';
+export { PageProvider, usePage, type PageSidebarSide } from './PageContext';
