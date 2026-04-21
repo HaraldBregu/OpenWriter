7e3ea40afe1b0b57d296f519c27a7532c47b72af
harald bregu <harald.bregu@gmail.com>
2026-04-21 15:38:35 +0200

    auto: add pageReducer in context/reducer module

---
+++ src/renderer/src/components/app/base/page/context/reducer.ts
@@ -0,0 +1,32 @@
+import type { PageState } from './state';
+import type { PageAction } from './actions';
+
+export function pageReducer(state: PageState, action: PageAction): PageState {
+	switch (action.type) {
+		case 'SIDEBAR_SHOWN':
+			return { ...state, isSidebarVisible: true };
+
+		case 'SIDEBAR_HIDDEN':
+			return { ...state, isSidebarVisible: false };
+
+		case 'SIDEBAR_TOGGLED':
+			return { ...state, isSidebarVisible: !state.isSidebarVisible };
+
+		case 'SIDEBAR_SIDE_SET':
+			return { ...state, sidebarSide: action.side };
+
+		case 'HEADER_SHOWN':
+			return { ...state, isHeaderVisible: true };
+
+		case 'HEADER_HIDDEN':
+			return { ...state, isHeaderVisible: false };
+
+		case 'HEADER_TOGGLED':
+			return { ...state, isHeaderVisible: !state.isHeaderVisible };
+
+		default: {
+			const _exhaustive: never = action;
+			return _exhaustive;
+		}
+	}
+}
