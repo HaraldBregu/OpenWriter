097da264188decd08c5460540e49d705b80354c3
harald bregu <harald.bregu@gmail.com>
2026-04-21 16:26:42 +0200

    auto: add insertPromptView to EditorElement interface

--- src/renderer/src/components/app/editor/Editor.tsx
+++ src/renderer/src/components/app/editor/Editor.tsx
@@ -39,6 +39,7 @@ export interface EditorElement extends HTMLDivElement {
 	setAssistantLoading: (loading: boolean) => void;
 	setAssistantEnable: (enable: boolean) => void;
 	clearPromptInput: () => void;
+	insertPromptView: () => void;
 	splitBlock: () => void;
 	setHeading: (level: 1 | 2 | 3 | 4 | 5 | 6) => void;
 	ensureBulletList: () => void;
