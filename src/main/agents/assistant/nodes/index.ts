152f4bb53e9ca642ec447fb66bdd40ba5a46659a
harald bregu <harald.bregu@gmail.com>
2026-04-21 13:11:26 +0200

    auto: add barrel export for assistant agent nodes

---
+++ src/main/agents/assistant/nodes/index.ts
@@ -0,0 +1,15 @@
+export type {
+	AssistantNode,
+	NodeContext,
+	NodeEvent,
+	NodeName,
+	NodeStatus,
+	NodeState,
+	Intent,
+	Order,
+	ImageNodeResult,
+} from './node';
+export { IntentNode } from './intent-node';
+export { PlannerNode } from './planner-node';
+export { ImageNode } from './image-node';
+export { TextNode } from './text-node';
