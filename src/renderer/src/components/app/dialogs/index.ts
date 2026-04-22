5f305b3df5ad0f4e156054fbdab6120a2e23dbde
harald bregu <harald.bregu@gmail.com>
2026-04-22 14:36:52 +0200

    export TasksDialog, ReduxStateDialog, LogDialog from dialogs index

--- src/renderer/src/components/app/dialogs/index.ts
+++ src/renderer/src/components/app/dialogs/index.ts
@@ -2,3 +2,6 @@ export { DeleteConfirmDialog } from './DeleteConfirmDialog';
 export type { DeleteConfirmDialogProps } from './DeleteConfirmDialog';
 export { InsertImageDialog } from './InsertImageDialog';
 export { ImagePreviewDialog } from './ImagePreviewDialog';
+export { TasksDialog } from './TasksDialog';
+export { ReduxStateDialog } from './ReduxStateDialog';
+export { LogDialog } from './LogDialog';
