f3ccacc3465fca4df0b44bb25f523ec76e0f251f
harald bregu <harald.bregu@gmail.com>
2026-04-22 23:42:32 +0200

    add writer agent barrel export

---
+++ src/main/agents/writer/index.ts
@@ -0,0 +1,10 @@
+export { WriterAgent } from './writer-agent';
+export type {
+	WriterAgentInput,
+	WriterAgentOutput,
+	WriterDecision,
+	WriterAction,
+	WriterIntent,
+	WriterIntentClassification,
+	WriterSkill,
+} from './types';
