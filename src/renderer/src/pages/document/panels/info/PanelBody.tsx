5ae5f8ad596d0d69d95bc93c0f2aadfd8e53a622
harald bregu <harald.bregu@gmail.com>
2026-04-22 10:54:51 +0200

    auto: remove PdfExportSection from info PanelBody

--- src/renderer/src/pages/document/panels/info/PanelBody.tsx
+++ src/renderer/src/pages/document/panels/info/PanelBody.tsx
@@ -1,30 +1,11 @@
-import { lazy, Suspense } from 'react';
-import { useTranslation } from 'react-i18next';
 import { CardContent } from '@/components/ui/Card';
-import { useDocumentState } from '../../hooks';
 import { ActionsSection } from './components/ActionsSection';
 import { DocumentMetaSection } from './components/DocumentMetaSection';
-import { useInfoState } from './hooks/use-info-state';
-
-const PdfExportSection = lazy(() => import('./components/PdfExportSection'));
 
 export function PanelBody(): React.ReactElement {
-	const { t } = useTranslation();
-	const { documentId } = useDocumentState();
-	const { documentConfig } = useInfoState();
-
 	return (
 		<CardContent className="flex-1 min-h-0 overflow-y-auto overflow-x-hidden pt-2 pb-6 space-y-2">
 			<DocumentMetaSection />
-			{documentId && documentConfig && (
-				<Suspense fallback={null}>
-					<PdfExportSection
-						exportLabel={t('configSidebar.exportPdf')}
-						downloadLabel={t('common.download')}
-						previewLabel={t('common.preview')}
-					/>
-				</Suspense>
-			)}
 			<ActionsSection />
 		</CardContent>
 	);
