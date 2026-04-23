dbf669f90b72c5934b7e879d7818a2799cfb754a
harald bregu <harald.bregu@gmail.com>
2026-04-23 23:56:48 +0200

    remove unused DemoTaskOutput export from task handlers index

--- src/main/task/handlers/index.ts
+++ src/main/task/handlers/index.ts
@@ -1,2 +1,2 @@
 export { DemoTaskHandler } from './demo-task-handler';
-export type { DemoTaskInput, DemoTaskOutput } from './demo-task-handler';
+export type { DemoTaskInput } from './demo-task-handler';
