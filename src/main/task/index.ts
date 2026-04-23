d17ed7dfaee23ab56b6b326937a2f1725c53ef12
harald bregu <harald.bregu@gmail.com>
2026-04-23 23:18:22 +0200

    remove unused type exports from task index

--- src/main/task/index.ts
+++ src/main/task/index.ts
@@ -2,6 +2,6 @@ export { TaskExecutor } from './task-executor';
 export { TaskHandlerRegistry } from './task-handler-registry';
 export { TaskReactionRegistry } from './task-reaction-registry';
 export { TaskReactionBus } from './task-reaction-bus';
-export type { TaskHandler, ProgressReporter, RecordEvent, AgentEvent } from './task-handler';
+export type { TaskHandler } from './task-handler';
 export type { ActiveTask, TaskOptions, TaskState, TaskPriority } from './task-descriptor';
 export type { TaskReactionHandler } from './task-reaction-handler';
