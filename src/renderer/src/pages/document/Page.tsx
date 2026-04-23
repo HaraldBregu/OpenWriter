9fd108d97e3df281a88b9ac9b71c2118c202da78
harald bregu <harald.bregu@gmail.com>
2026-04-24 00:24:02 +0200

    remove phaseLabel prop from TaskStatusBar usage

--- src/renderer/src/pages/document/Page.tsx
+++ src/renderer/src/pages/document/Page.tsx
@@ -562,7 +562,7 @@ function PageContent(): ReactElement {
 									<DocumentInfoPopover documentId={id ?? null} title={title} content={content} />
 								</PageHeaderItems>
 							</PageHeader>
-							<TaskStatusBar taskId={activeTaskId} phaseLabel={phaseLabel} />
+							<TaskStatusBar taskId={activeTaskId} />
 							<div className="flex min-h-0 flex-1 flex-col">
 								{loaded && (
 									<Editor
