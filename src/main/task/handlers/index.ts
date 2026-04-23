cc37f041088f551e9b3f9e0b7b2abe6b9f3bafbf
harald bregu <harald.bregu@gmail.com>
2026-04-23 23:15:54 +0200

    remove non-demo task handler exports from handlers index

--- src/main/task/handlers/index.ts
+++ src/main/task/handlers/index.ts
@@ -1,21 +1,2 @@
 export { DemoTaskHandler } from './demo-task-handler';
 export type { DemoTaskInput, DemoTaskOutput } from './demo-task-handler';
-export { AgentTaskHandler } from './agent-task-handler';
-export type { AgentTaskInput } from './agent-task-handler';
-export { TranscriptionTaskHandler } from './transcription-task-handler';
-export type { TranscriptionTaskInput } from './transcription-task-handler';
-export { ImageGeneratorTaskHandler } from './image-generator-task-handler';
-export type {
-	ImageGeneratorTaskInput,
-	ImageGeneratorTaskOutput,
-	ImageGeneratorAgentInput,
-	GeneratedImage,
-	ImageResponseFormat,
-	ImageSize,
-	ImageQuality,
-	ImageStyle,
-} from './image-generator-task-handler';
-export { TextGeneratorV1TaskHandler } from './text-generator-v1-task-handler';
-export type { TextGeneratorV1TaskInput } from './text-generator-v1-task-handler';
-export { TextGeneratorV2TaskHandler } from './text-generator-v2-task-handler';
-export type { TextGeneratorV2TaskInput } from './text-generator-v2-task-handler';
