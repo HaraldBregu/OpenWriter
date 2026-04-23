5e86fd9d08c7de5e4c682c3ec5ab575a33a7d44b
harald bregu <harald.bregu@gmail.com>
2026-04-23 22:24:40 +0200

    change TASK_TYPE to demo in document Page

--- src/renderer/src/pages/document/Page.tsx
+++ src/renderer/src/pages/document/Page.tsx
@@ -38,7 +38,7 @@ import { PromptSubmitPayload } from '@shared/index';
 
 const METADATA_SAVE_DEBOUNCE_MS = 500;
 const CONTENT_SAVE_DEBOUNCE_MS = 1500;
-const TASK_TYPE = 'text-generator-v2';
+const TASK_TYPE = 'demo';
 
 function PageContent(): ReactElement {
 	const { documentId: id, selection } = useDocumentState();
