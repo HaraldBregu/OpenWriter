bc431dbdec0549ddfe460b813e0292696ca8f0f3
harald bregu <harald.bregu@gmail.com>
2026-04-23 15:29:50 +0200

    rename AGENT_TYPE from writer to text-writer in document Page

--- src/renderer/src/pages/document/Page.tsx
+++ src/renderer/src/pages/document/Page.tsx
@@ -44,7 +44,7 @@ import { PromptSubmitPayload } from '@shared/index';
 
 const METADATA_SAVE_DEBOUNCE_MS = 500;
 const CONTENT_SAVE_DEBOUNCE_MS = 1500;
-const AGENT_TYPE = 'writer';
+const AGENT_TYPE = 'text-writer';
 
 function PageContent(): ReactElement {
 	const { documentId: id, selection } = useDocumentState();
