d3e275ee34c2108afd31a6337c1b9ff60724dcbf
HaraldBregu <harald.bregu@gmail.com>
2026-04-25 22:29:36 +0200

    export PageSidebarLayout primitives from page index

--- src/renderer/src/components/app/base/page/index.ts
+++ src/renderer/src/components/app/base/page/index.ts
@@ -9,6 +9,31 @@ export {
 	PageSidebar,
 	PageSidebarInset,
 } from './Page';
+export {
+	PageSidebarLayout,
+	PageSidebarLayoutContainer,
+	PageSidebarLayoutContent,
+	PageSidebarLayoutFooter,
+	PageSidebarLayoutGroup,
+	PageSidebarLayoutGroupAction,
+	PageSidebarLayoutGroupContent,
+	PageSidebarLayoutGroupLabel,
+	PageSidebarLayoutHeader,
+	PageSidebarLayoutInput,
+	PageSidebarLayoutInset,
+	PageSidebarLayoutMenu,
+	PageSidebarLayoutMenuAction,
+	PageSidebarLayoutMenuBadge,
+	PageSidebarLayoutMenuButton,
+	PageSidebarLayoutMenuItem,
+	PageSidebarLayoutMenuSkeleton,
+	PageSidebarLayoutMenuSub,
+	PageSidebarLayoutMenuSubButton,
+	PageSidebarLayoutMenuSubItem,
+	PageSidebarLayoutRail,
+	PageSidebarLayoutSeparator,
+	PageSidebarLayoutTrigger,
+} from './PageSidebarLayout';
 export { Provider } from './Provider';
 export { usePageContext } from './hooks';
 export {
