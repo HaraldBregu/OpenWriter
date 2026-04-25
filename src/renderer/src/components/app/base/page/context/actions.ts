94d10b75063b8e22a3fa3d154bc76d16cfe0aba1
HaraldBregu <harald.bregu@gmail.com>
2026-04-25 22:27:10 +0200

    add sidebar open and mobile open actions to PageAction

--- src/renderer/src/components/app/base/page/context/actions.ts
+++ src/renderer/src/components/app/base/page/context/actions.ts
@@ -7,4 +7,8 @@ export type PageAction =
 	| { type: 'SIDEBAR_SIDE_SET'; side: SidebarSide }
 	| { type: 'HEADER_SHOWN' }
 	| { type: 'HEADER_HIDDEN' }
-	| { type: 'HEADER_TOGGLED' };
+	| { type: 'HEADER_TOGGLED' }
+	| { type: 'SIDEBAR_OPEN_SET'; open: boolean }
+	| { type: 'SIDEBAR_OPEN_TOGGLED' }
+	| { type: 'SIDEBAR_OPEN_MOBILE_SET'; open: boolean }
+	| { type: 'SIDEBAR_OPEN_MOBILE_TOGGLED' };
