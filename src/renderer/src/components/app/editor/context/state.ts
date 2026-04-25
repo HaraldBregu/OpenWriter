da101dfd987d198d54c0a3527b9aa415088b0e39
HaraldBregu <harald.bregu@gmail.com>
2026-04-25 23:40:40 +0200

    remove unused HoveredBlock interface and hoveredBlock state

--- src/renderer/src/components/app/editor/context/state.ts
+++ src/renderer/src/components/app/editor/context/state.ts
@@ -1,10 +1,3 @@
-export interface HoveredBlock {
-	node: HTMLElement;
-	pos: number;
-	top: number;
-}
-
 export interface EditorState {
-	hoveredBlock: HoveredBlock | null;
 	imageDialogOpen: boolean;
 }
