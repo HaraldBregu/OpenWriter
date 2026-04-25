1fd3136516da213347ae3a53fcc6ef135f610e25
HaraldBregu <harald.bregu@gmail.com>
2026-04-25 23:41:17 +0200

    remove unused SET_HOVERED_BLOCK case from editorReducer

--- src/renderer/src/components/app/editor/context/reducer.ts
+++ src/renderer/src/components/app/editor/context/reducer.ts
@@ -3,8 +3,6 @@ import type { EditorAction } from './actions';
 
 export function editorReducer(state: EditorState, action: EditorAction): EditorState {
 	switch (action.type) {
-		case 'SET_HOVERED_BLOCK':
-			return { ...state, hoveredBlock: action.payload };
 		case 'SET_IMAGE_DIALOG_OPEN':
 			return { ...state, imageDialogOpen: action.payload };
 	}
