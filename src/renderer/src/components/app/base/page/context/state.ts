bbf434aa24204e861852dac2ee578153bb07ad53
HaraldBregu <harald.bregu@gmail.com>
2026-04-25 22:26:55 +0200

    add sidebarOpen and sidebarOpenMobile to PageState

--- src/renderer/src/components/app/base/page/context/state.ts
+++ src/renderer/src/components/app/base/page/context/state.ts
@@ -4,10 +4,14 @@ export interface PageState {
 	readonly isSidebarVisible: boolean;
 	readonly sidebarSide: SidebarSide;
 	readonly isHeaderVisible: boolean;
+	readonly sidebarOpen: boolean;
+	readonly sidebarOpenMobile: boolean;
 }
 
 export const INITIAL_PAGE_STATE: PageState = {
 	isSidebarVisible: true,
 	sidebarSide: 'left',
 	isHeaderVisible: true,
+	sidebarOpen: true,
+	sidebarOpenMobile: false,
 };
