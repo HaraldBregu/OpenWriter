be1c46fa9022170347a3196e0135ddfcb7822b55
HaraldBregu <harald.bregu@gmail.com>
2026-04-27 16:46:51 +0200

    export ErrorDialog from dialogs barrel

--- src/renderer/src/components/app/dialogs/index.ts
+++ src/renderer/src/components/app/dialogs/index.ts
@@ -1,5 +1,7 @@
 export { DeleteConfirmDialog } from './DeleteConfirmDialog';
 export type { DeleteConfirmDialogProps } from './DeleteConfirmDialog';
+export { ErrorDialog } from './ErrorDialog';
+export type { ErrorDialogProps } from './ErrorDialog';
 export { InsertImageDialog } from './InsertImageDialog';
 export { ImagePreviewDialog } from './ImagePreviewDialog';
 export { TasksDialog } from './TasksDialog';
