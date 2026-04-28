c456c5951515106ecabb30b3469c438bbf133b62
harald bregu <harald.bregu@gmail.com>
2026-04-28 11:09:49 +0200

    remove ExtensionsApi type import from preload index

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -7,7 +7,7 @@ import {
 	TaskChannels,
 	LogChannels,
 } from '../shared/channels';
-import type { AppApi, ExtensionsApi, WindowApi, WorkspaceApi, TaskApi } from './index.d';
+import type { AppApi, WindowApi, WorkspaceApi, TaskApi } from './index.d';
 import type {
 	AgentSettings,
 	ContextMenuDescriptor,
