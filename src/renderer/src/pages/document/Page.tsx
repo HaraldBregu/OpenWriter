e1d9b7830471ccedaabad9ac5254c3b7ebc3aca6
harald bregu <harald.bregu@gmail.com>
2026-04-28 16:11:22 +0200

    remove unused selection from usePromptActions params

--- src/renderer/src/pages/document/Page.tsx
+++ src/renderer/src/pages/document/Page.tsx
@@ -205,7 +205,6 @@ function PageContent(): ReactElement {
 		documentId: id ?? null,
 		editor,
 		editorActions,
-		selection,
 		isExternallyBusy: documentHasActiveTask || preexistingTaskActive,
 		onMarkdownChanged: (markdown) => {
 			setContent(markdown);
