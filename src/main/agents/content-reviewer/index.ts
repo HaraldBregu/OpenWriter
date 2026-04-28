4a834d1a5c9f0619e00c654a8c81f24231c26b0f
harald bregu <harald.bregu@gmail.com>
2026-04-28 23:54:59 +0200

    add content-reviewer agent barrel exports

---
+++ src/main/agents/content-reviewer/index.ts
@@ -0,0 +1,8 @@
+export { ContentReviewerAgent } from './content-reviewer-agent';
+export type { ContentReviewerAgentOptions } from './content-reviewer-agent';
+export type {
+	ContentReviewerAgentInput,
+	ContentReviewerAgentOutput,
+	ContentReviewerLlmCaller,
+	ContentReviewerStreamParams,
+} from './types';
