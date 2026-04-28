8eaf6629e103dda04aa074eff38b6ff643e9a340
harald bregu <harald.bregu@gmail.com>
2026-04-29 00:02:46 +0200

    add ContentReviewerTaskHandler exports to handlers barrel

--- src/main/task/handlers/index.ts
+++ src/main/task/handlers/index.ts
@@ -2,3 +2,5 @@ export { DemoTaskHandler } from './demo-task-handler';
 export type { DemoTaskInput } from './demo-task-handler';
 export { ContentWriterTaskHandler } from './content-writer-task-handler';
 export type { ContentWriterTaskInput } from './content-writer-task-handler';
+export { ContentReviewerTaskHandler } from './content-reviewer-task-handler';
+export type { ContentReviewerTaskInput } from './content-reviewer-task-handler';
