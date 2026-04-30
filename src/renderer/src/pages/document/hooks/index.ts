75709d9ff96098f7025434baf2cde38552ff7377
harald bregu <harald.bregu@gmail.com>
2026-04-30 22:34:33 +0200

    remove useInsertContentDialog export from document hooks index

--- src/renderer/src/pages/document/hooks/index.ts
+++ src/renderer/src/pages/document/hooks/index.ts
@@ -1,6 +1,5 @@
 export { useContext } from './use-context';
 export { useEditorInstance } from './use-editor-instance';
-export { useInsertContentDialog } from './use-insert-content-dialog';
 export { useState } from './use-state';
 export { useDispatch } from './use-dispatch';
 export { useUI } from './use-ui';
