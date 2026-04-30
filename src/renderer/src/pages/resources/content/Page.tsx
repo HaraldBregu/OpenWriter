1a07bbe3cca1a0384f4a7ed35ad076f96e464637
harald bregu <harald.bregu@gmail.com>
2026-04-30 14:13:32 +0200

    replace DataTableViewOptions with inline DropdownMenu for column toggle

--- src/renderer/src/pages/resources/content/Page.tsx
+++ src/renderer/src/pages/resources/content/Page.tsx
@@ -200,7 +200,42 @@ function PageContent(): ReactElement {
 								}
 								className="max-w-sm"
 							/>
-							<DataTableViewOptions table={table} />
+							<DropdownMenu>
+								<DropdownMenuTrigger
+									render={
+										<Button
+											variant="outline"
+											size="sm"
+											className="ml-auto hidden h-8 lg:flex"
+										/>
+									}
+								>
+									<Settings2 className="mr-2 h-4 w-4" />
+									View
+								</DropdownMenuTrigger>
+								<DropdownMenuContent align="end" className="w-[150px]">
+									<DropdownMenuGroup>
+										<DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
+										<DropdownMenuSeparator />
+										{table
+											.getAllColumns()
+											.filter(
+												(column) =>
+													typeof column.accessorFn !== 'undefined' && column.getCanHide(),
+											)
+											.map((column) => (
+												<DropdownMenuCheckboxItem
+													key={column.id}
+													className="capitalize"
+													checked={column.getIsVisible()}
+													onCheckedChange={(value) => column.toggleVisibility(!!value)}
+												>
+													{column.id}
+												</DropdownMenuCheckboxItem>
+											))}
+									</DropdownMenuGroup>
+								</DropdownMenuContent>
+							</DropdownMenu>
 						</div>
 						<div className="overflow-hidden rounded-md border">
 							<Table>
