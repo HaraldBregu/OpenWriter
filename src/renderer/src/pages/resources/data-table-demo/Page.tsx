072263d7973937f419ae7b7256d204e023ce2462
harald bregu <harald.bregu@gmail.com>
2026-04-30 12:12:48 +0200

    wrap DataTableDemoPage in PageContainer layout

--- src/renderer/src/pages/resources/data-table-demo/Page.tsx
+++ src/renderer/src/pages/resources/data-table-demo/Page.tsx
@@ -1,13 +1,27 @@
 import type { ReactElement } from 'react';
 
+import {
+	PageBody,
+	PageContainer,
+	PageHeader,
+	PageHeaderTitle,
+} from '@/components/app/base/page';
+import { Label } from '@/components/ui/Label';
 import { columns } from './columns';
 import { payments } from './data';
 import { DataTable } from './DataTable';
 
 export default function DataTableDemoPage(): ReactElement {
 	return (
-		<div className="container mx-auto py-10">
-			<DataTable columns={columns} data={payments} />
-		</div>
+		<PageContainer>
+			<PageHeader>
+				<PageHeaderTitle>
+					<Label className="w-full text-left text-sm font-medium">Data Table</Label>
+				</PageHeaderTitle>
+			</PageHeader>
+			<PageBody>
+				<DataTable columns={columns} data={payments} />
+			</PageBody>
+		</PageContainer>
 	);
 }
