bec8d989ed730ea2a189d5898b244b3fc3a8f50b
harald bregu <harald.bregu@gmail.com>
2026-04-30 22:33:42 +0200

    remove insertContentDialog fields from ContextValue

--- src/renderer/src/pages/document/context/context.ts
+++ src/renderer/src/pages/document/context/context.ts
@@ -8,9 +8,6 @@ export interface ContextValue {
 	dispatch: Dispatch<DocumentAction>;
 	editor: Editor | null;
 	setEditor: (editor: Editor | null) => void;
-	insertContentDialogOpen: boolean;
-	openInsertContentDialog: () => void;
-	closeInsertContentDialog: () => void;
 }
 
 export const DocumentContext = createContext<ContextValue | null>(null);
