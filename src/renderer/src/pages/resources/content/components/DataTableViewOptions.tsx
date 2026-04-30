35ba79b38b3c257903efadb49be0707fc335dfc9
harald bregu <harald.bregu@gmail.com>
2026-04-30 12:20:36 +0200

    add DataTableViewOptions component for toggling column visibility

---
+++ src/renderer/src/pages/resources/content/components/DataTableViewOptions.tsx
@@ -0,0 +1,47 @@
+import type { Table } from '@tanstack/react-table';
+import { Settings2 } from 'lucide-react';
+
+import { Button } from '@/components/ui/Button';
+import {
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/DropdownMenu';
+
+export function DataTableViewOptions<TData>({ table }: { table: Table<TData> }) {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger
+				render={
+					<Button variant="outline" size="sm" className="ml-auto hidden h-8 lg:flex" />
+				}
+			>
+				<Settings2 className="mr-2 h-4 w-4" />
+				View
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align="end" className="w-[150px]">
+				<DropdownMenuGroup>
+					<DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					{table
+						.getAllColumns()
+						.filter((column) => typeof column.accessorFn !== 'undefined' && column.getCanHide())
+						.map((column) => (
+							<DropdownMenuCheckboxItem
+								key={column.id}
+								className="capitalize"
+								checked={column.getIsVisible()}
+								onCheckedChange={(value) => column.toggleVisibility(!!value)}
+							>
+								{column.id}
+							</DropdownMenuCheckboxItem>
+						))}
+				</DropdownMenuGroup>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+}
