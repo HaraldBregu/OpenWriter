8cc555f6e66ed6f7ba73a85a1f7dc134fd2d21f5
harald bregu <harald.bregu@gmail.com>
2026-05-01 19:23:21 +0200

    remove footer user email from sidebar menu button

--- src/renderer/src/components/app/base/Layout.tsx
+++ src/renderer/src/components/app/base/Layout.tsx
@@ -613,7 +613,6 @@ function Container({ children }: LayoutProps) {
 												</Avatar>
 												<div className="grid flex-1 text-left text-sm leading-tight">
 													<span className="truncate font-medium">{footerUserName}</span>
-													<span className="truncate text-xs">{footerUserEmail}</span>
 												</div>
 												<EllipsisVertical className="ml-auto size-4" />
 											</SidebarMenuButton>
