1d6e927784b95df2c27db1bf9852cadbc7650ac9
harald bregu <harald.bregu@gmail.com>
2026-05-02 20:31:37 +0200

    import CronService in bootstrap

--- src/main/bootstrap.ts
+++ src/main/bootstrap.ts
@@ -21,6 +21,7 @@ import { ServiceContainer, EventBus, WindowFactory, AppState, WindowContextManag
 import { StoreService } from './services/store';
 import { LoggerService } from './services/logger';
 import { ThemeService } from './services/theme-service';
+import { CronService } from './services/cron-service';
 import { SkillsStoreService } from './services/skills-store-service';
 import { StreamLoggerService } from './services/stream-logger';
 import { FileManager } from './shared/file_manager';
