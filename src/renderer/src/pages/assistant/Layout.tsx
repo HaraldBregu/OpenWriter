8ee77bfce0f955ecc2212991682c7d4e88cc2607
harald bregu <harald.bregu@gmail.com>
2026-05-02 22:30:02 +0200

    add assistant layout component

---
+++ src/renderer/src/pages/assistant/Layout.tsx
@@ -0,0 +1,10 @@
+import type { ReactElement, ReactNode } from 'react';
+import { Provider } from './Provider';
+
+interface LayoutProps {
+	readonly children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps): ReactElement {
+	return <Provider>{children}</Provider>;
+}
