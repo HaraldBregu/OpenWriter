54f7a17a94cb3798b9e07777bbb6c2e9159b860d
harald bregu <harald.bregu@gmail.com>
2026-05-02 11:35:00 +0200

    widen ConfigPage sidebar to 3/5 with lg:2/3

--- src/renderer/src/pages/welcome/ConfigPage.tsx
+++ src/renderer/src/pages/welcome/ConfigPage.tsx
@@ -80,7 +80,7 @@ const ConfigPage: React.FC<ConfigPageProps> = ({ onConfigured }) => {
 			<TitleBar title="OpenWriter" />
 
 			<PageBody className="p-0 flex-row">
-				<aside className="hidden md:flex flex-col items-center justify-center gap-5 w-1/2 px-10 py-12 border-r bg-muted/30">
+				<aside className="hidden md:flex flex-col items-center justify-center gap-5 w-3/5 lg:w-2/3 px-10 py-12 border-r bg-muted/30">
 					<AppIconOpenWriter
 						className="text-foreground"
 						style={{
