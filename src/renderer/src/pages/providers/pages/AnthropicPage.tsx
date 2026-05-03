6921db09fd400d1a29af752899a8d1846a76e648
harald bregu <harald.bregu@gmail.com>
2026-05-03 23:11:35 +0200

    align anthropic page card footer to end

--- src/renderer/src/pages/providers/pages/AnthropicPage.tsx
+++ src/renderer/src/pages/providers/pages/AnthropicPage.tsx
@@ -73,7 +73,7 @@ export default function AnthropicPage(): ReactElement {
 						</Field>
 					</FieldGroup>
 				</CardContent>
-				<CardFooter>
+				<CardFooter className="justify-end">
 					<Button type="submit" disabled={!isDirty || isSaving}>
 						{isSaving ? <Spinner /> : <Save />}
 						{t('common.save', 'Save')}
