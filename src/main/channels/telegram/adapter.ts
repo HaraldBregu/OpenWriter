9013afc1479ab132a6fa2fa9ed3d8a9cfece081d
harald bregu <harald.bregu@gmail.com>
2026-05-03 16:15:31 +0200

    add onMessage handler and dispatch to TelegramAdapter

--- src/main/channels/telegram/adapter.ts
+++ src/main/channels/telegram/adapter.ts
@@ -2,15 +2,27 @@ import { Bot } from "grammy";
 import { registerTextHandler } from "./receive";
 import { sendChunked } from "./send";
 import type { TelegramAdapterOptions } from "./types";
+import type { ChannelMessage, ChannelMessageHandler } from "../types";
 
 export class TelegramAdapter {
   private bot: Bot;
   private allowFrom: Set<string>;
+  private handlers = new Set<ChannelMessageHandler>();
 
   constructor(opts: TelegramAdapterOptions) {
     this.bot = new Bot(opts.token);
     this.allowFrom = new Set(opts.allowFrom.map(String));
-    registerTextHandler(this.bot, this.allowFrom);
+    registerTextHandler(this.bot, this.allowFrom, ({ from, chatId, text }) => {
+      const msg: ChannelMessage = { type: "TELEGRAM", from, chatId, text };
+      for (const h of this.handlers) h(msg);
+    });
+  }
+
+  onMessage(handler: ChannelMessageHandler): () => void {
+    this.handlers.add(handler);
+    return () => {
+      this.handlers.delete(handler);
+    };
   }
 
   async send(chatId: string, text: string): Promise<void> {
