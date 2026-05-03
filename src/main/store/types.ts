3a5af6e72148b8863444cb5feae31520f9f402d3
harald bregu <harald.bregu@gmail.com>
2026-05-03 15:27:56 +0200

    rename channels field to channel in StoreSchema

--- src/main/store/types.ts
+++ src/main/store/types.ts
@@ -8,7 +8,7 @@ export interface WorkspaceInfo {
 export interface StoreSchema {
 	providers: Provider[];
 	agents: AgentSettings[];
-	channels: ChannelMap;
+	channel: ChannelMap;
 	currentWorkspace: string | null;
 	recentWorkspaces: WorkspaceInfo[];
 	startupCount: number;
