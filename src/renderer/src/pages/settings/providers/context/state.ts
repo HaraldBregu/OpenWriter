131bdb80112d24e25c6ed6edd01950070e5248d9
harald bregu <harald.bregu@gmail.com>
2026-05-05 20:23:59 +0200

    add providers context state module

---
+++ src/renderer/src/pages/settings/providers/context/state.ts
@@ -0,0 +1,28 @@
+import type { Provider, ProviderId } from '../../../../../../shared/types';
+import { PROVIDER_IDS } from '../../../../../../shared/providers';
+
+export interface DraftProperties {
+	readonly apiKey: string;
+}
+
+export const EMPTY_DRAFT: DraftProperties = { apiKey: '' };
+
+export type DraftsByProvider = Record<ProviderId, DraftProperties>;
+
+function emptyDrafts(): DraftsByProvider {
+	const map = {} as DraftsByProvider;
+	for (const id of PROVIDER_IDS) map[id] = EMPTY_DRAFT;
+	return map;
+}
+
+export interface ProvidersState {
+	providers: Provider[];
+	drafts: DraftsByProvider;
+	saving: ReadonlySet<ProviderId>;
+}
+
+export const initialState: ProvidersState = {
+	providers: [],
+	drafts: emptyDrafts(),
+	saving: new Set(),
+};
