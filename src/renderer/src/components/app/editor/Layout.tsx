ffc5f90b4247c35f5c8402e3f4054b4b543f057f
HaraldBregu <harald.bregu@gmail.com>
2026-05-06 18:20:35 +0200

    use inline style for Layout maxWidth instead of dynamic class

--- src/renderer/src/components/app/editor/Layout.tsx
+++ src/renderer/src/components/app/editor/Layout.tsx
@@ -20,7 +20,8 @@ const Layout = forwardRef<HTMLDivElement, LayoutProps>(
 			<div id={id} className={cn('h-full min-w-0 flex flex-col', className)}>
 				<div className="flex-1 overflow-y-auto overflow-x-hidden bg-background">
 					<div
-						className={`mx-auto flex w-full max-w-[${width}%] flex-col gap-2 px-10 py-10`}
+						className="mx-auto flex w-full flex-col gap-2 px-10 py-10"
+						style={{ maxWidth: `${width}%` }}
 					>
 						<div className="relative w-full" ref={ref}>
 							<div
