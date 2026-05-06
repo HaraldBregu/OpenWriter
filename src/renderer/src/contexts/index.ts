d16894ca08c3d7af6b04c2a99a6b32e7559515c9
HaraldBregu <harald.bregu@gmail.com>
2026-05-06 18:53:29 +0200

    export EditorPrefsProvider in contexts index instead of EditorWidthProvider

--- src/renderer/src/contexts/index.ts
+++ src/renderer/src/contexts/index.ts
@@ -3,8 +3,8 @@ export { ImagesProvider, useImagesContext } from './ImagesProvider';
 export type { ImagesContextValue } from './ImagesProvider';
 export { ContentProvider, useContentContext } from './ContentProvider';
 export type { ContentContextValue } from './ContentProvider';
-export { EditorWidthProvider, useEditorWidthContext } from './EditorWidthProvider';
-export type { EditorWidthContextValue } from './EditorWidthProvider';
+export { EditorPrefsProvider, useEditorPrefsContext } from './EditorPrefsProvider';
+export type { EditorPrefsContextValue } from './EditorPrefsProvider';
 
 export type {
 	AppLanguage,
